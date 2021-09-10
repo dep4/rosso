f680a5e2b3f8c21efb132a91d4522ed617b6c364
Steven Penny <srpen6@gmail.com>
2021-09-10 13:25:22 -0500

    new file:   research/count_test.go

--- research/count.go
+++ research/count.go
@@ -2,12 +2,15 @@ package sort
 
 import (
    "encoding/json"
+   "fmt"
    "io"
+   "sort"
 )
 
 const allUas = "https://ja3er.com/getAllUasJson"
 
 type counts []struct {
+   LastSeen string `json:"Last_seen"`
    MD5 string
    UserAgent string `json:"User-Agent"`
 }
@@ -20,18 +23,54 @@ func newCounts(r io.Reader) (counts, error) {
    return c, nil
 }
 
+func (c counts) groups() groups {
+   var g groups
+   m := make(map[string]int)
+   for i, count := range c {
+      fmt.Printf("%9v\r", len(c)-i)
+      j, ok := m[count.MD5]
+      if ok {
+         g[j].agents = append(g[j].agents, count.UserAgent)
+      } else {
+         g = append(g, group{
+            count.MD5, []string{count.UserAgent},
+         })
+         m[count.MD5] = len(g)-1
+      }
+   }
+   return g
+}
+
 type group struct {
    md5 string
    agents []string
 }
 
-type groups []group
-
-func (g groups) index(md5 string) int {
-   for k, v := range g {
-      if v.md5 == md5 {
-         return k
+func (c counts) filter(lastSeen string) counts {
+   var d counts
+   for _, count := range c {
+      if count.LastSeen > lastSeen {
+         d = append(d, count)
       }
    }
-   return -1
+   return d
+}
+
+type groups []group
+
+func (g groups) filter(agents int) groups {
+   var h groups
+   for _, group := range g {
+      if len(group.agents) > agents {
+         h = append(h, group)
+      }
+   }
+   return h
+}
+
+func (g groups) sort() {
+   sort.Slice(g, func(a, b int) bool {
+      ga, gb := g[a], g[b]
+      return len(gb.agents) < len(ga.agents)
+   })
 }
