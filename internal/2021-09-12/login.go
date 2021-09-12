192cae839160bdbc51b90584bc48ed1a42a11408
Steven Penny <srpen6@gmail.com>
2021-09-12 14:54:34 -0500

    new file:   internal/2021-09-10/login.go

--- internal/2021-09-12/login.go
+++ internal/2021-09-12/login.go
@@ -4,83 +4,22 @@ import (
    "fmt"
    "github.com/89z/parse/ja3"
    "net/http"
-   "net/url"
-   "os"
-   "strings"
-   "time"
 )
 
-func main() {
-   pass := os.Getenv("ENCRYPTEDPASS")
-   if pass == "" {
-      fmt.Println("missing pass")
-      return
-   }
-   val := url.Values{
-      "Email": {"srpen6@gmail.com"},
-      "sdk_version": {"17"},
-      "EncryptedPasswd": {pass},
-   }
-   ua, err := os.Open("getAllUasJson.json")
-   if err != nil {
-      panic(err)
-   }
-   defer ua.Close()
-   hash, err := os.Open("getAllHashesJson.json")
-   if err != nil {
-      panic(err)
-   }
-   defer hash.Close()
-   j, err := ja3.NewJA3er(ua, hash)
-   if err != nil {
-      panic(err)
-   }
-   j.SortUsers()
-   for _, user := range j.Users {
-      fmt.Println(user)
-      hello := j.JA3(user.MD5)
-      spec, err := ja3.Parse(hello)
-      if err != nil {
-         panic(err)
-      }
-      req, err := http.NewRequest(
-         "POST", "https://android.clients.google.com/auth",
-         strings.NewReader(val.Encode()),
-      )
-      if err != nil {
-         panic(err)
-      }
-      req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
-      res, err := ja3.NewTransport(spec).RoundTrip(req)
-      if err != nil {
-         err := sanityCheck(hello)
-         if err != nil {
-            fmt.Println(err)
-            continue
-         }
-      }
-      defer res.Body.Close()
-      fmt.Println(res.Status)
-      if res.StatusCode == http.StatusOK {
-         break
-      }
-      time.Sleep(time.Second)
-   }
+var tests = []string{
+   "https://www.reddit.com",
+   "https://github.com",
+   "https://nebulance.io",
+   "https://stackoverflow.com",
+   "https://variety.com",
+   "https://vimeo.com",
+   "https://www.google.com",
+   "https://www.indiewire.com",
+   "https://www.wikipedia.org",
+   "https://www.youtube.com",
 }
 
 func sanityCheck(hello string) error {
-   tests := []string{
-      "https://www.reddit.com",
-      "https://github.com",
-      "https://nebulance.io",
-      "https://stackoverflow.com",
-      "https://variety.com",
-      "https://vimeo.com",
-      "https://www.google.com",
-      "https://www.indiewire.com",
-      "https://www.wikipedia.org",
-      "https://www.youtube.com",
-   }
    for _, test := range tests {
       spec, err := ja3.Parse(hello)
       if err != nil {
@@ -95,4 +34,7 @@ func sanityCheck(hello string) error {
       }
    }
    return fmt.Errorf("manual review %q", hello)
+}
+
+func main() {
 }
