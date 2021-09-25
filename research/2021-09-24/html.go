cf9a1fe14e2f440e48195a0360eb462c56d47186
Steven Penny <srpen6@gmail.com>
2021-09-24 20:17:04 -0500

    deleted:    html/render.go

--- research/2021-09-24/html.go
+++ research/2021-09-24/html.go
@@ -1,8 +1,8 @@
 // HTML
-
+//
 // Why use "github.com/tdewolff/parse/v2/html" instead of
 // "golang.org/x/net/html"?
-
+//
 // "go.sum" with "golang.org/x/net/html" looks like this:
 //  golang.org/x/net v0.0.0-20210924151903
 //  golang.org/x/sys v0.0.0-20201119102817
@@ -10,8 +10,44 @@
 //  golang.org/x/term v0.0.0-20201126162022
 //  golang.org/x/text v0.3.6
 //  golang.org/x/tools v0.0.0-20180917221912
-
+//
 // "go.sum" with "github.com/tdewolff/parse/v2/html" looks like this:
 //  github.com/tdewolff/parse/v2 v2.5.21
 //  github.com/tdewolff/test v1.0.6
+//
+// and because "tdewolff" also has a JavaScript parser, and "golang.org" does
+// not.
 package html
+
+import (
+   "github.com/tdewolff/parse/v2"
+   "github.com/tdewolff/parse/v2/html"
+   "io"
+)
+
+type lexer struct {
+   *html.Lexer
+}
+
+func newLexer(r io.Reader) lexer {
+   return lexer{
+      html.NewLexer(parse.NewInput(r)),
+   }
+}
+
+func (l lexer) tagName() string {
+   return string(l.Text())
+}
+
+func (l lexer) nextTag(name string) bool {
+   for {
+      switch tt, _ := l.Next(); tt {
+      case html.ErrorToken:
+         return false
+      case html.StartTagToken:
+         if l.tagName() == name {
+            return true
+         }
+      }
+   }
+}
--- research/2021-09-24/old.go
+++ research/2021-09-24/old.go
@@ -1,27 +1,12 @@
 package html
+import "github.com/tdewolff/parse/v2/html"
 
-import (
-   "github.com/tdewolff/parse/v2"
-   "github.com/tdewolff/parse/v2/html"
-   "io"
-)
-
-// godocs.io/github.com/tdewolff/parse/v2/html#Lexer
 type Lexer struct {
    *html.Lexer
    html.TokenType
    data []byte
-   attr map[string]string
 }
 
-// godocs.io/github.com/tdewolff/parse/v2/html#NewLexer
-func NewLexer(r io.Reader) Lexer {
-   return Lexer{
-      Lexer: html.NewLexer(parse.NewInput(r)),
-   }
-}
-
-// developer.mozilla.org/docs/Web/API/Node/textContent
 func (l *Lexer) Bytes() []byte {
    for {
       switch l.TokenType {
