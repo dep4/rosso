012726c37bf44d058eb684192ac2347fc5994b0e
Steven Penny <srpen6@gmail.com>
2021-10-27 16:55:57 -0500

    deleted:    research/2021-10-27/dreadl0ck.go

--- research/2021-10-27/open-ch.go
+++ research/2021-10-27/open-ch.go
@@ -1,10 +1,14 @@
-package main
+package tls
 
 import (
+   "encoding/binary"
    "github.com/89z/parse/tls"
+   "io"
+   "strconv"
+   utls "github.com/refraction-networking/utls"
 )
 
-func value(ext tls.TLSExtension) (uint16, error) {
+func value(ext utls.TLSExtension) (uint16, error) {
    data, err := io.ReadAll(ext)
    if err != nil {
       return 0, err
@@ -32,11 +36,21 @@ func marshalJA3(hello *tls.ClientHello) []byte {
       data[len(data)-1] = ','
    }
    // Extensions
+   var (
+      supportedCurves []utls.CurveID
+      supportedPoints []uint8
+   )
    if len(hello.Extensions) == 0 {
       data = append(data, ',')
    } else {
-      for _, ext := range hello.Extensions {
-         val, err := value(ext)
+      for _, iExt := range hello.Extensions {
+         switch sExt := iExt.(type) {
+         case *utls.SupportedCurvesExtension:
+            supportedCurves = sExt.Curves
+         case *utls.SupportedPointsExtension:
+            supportedPoints = sExt.SupportedPoints
+         }
+         val, err := value(iExt)
          if err != nil {
             return nil
          }
@@ -47,19 +61,19 @@ func marshalJA3(hello *tls.ClientHello) []byte {
       data[len(data)-1] = ','
    }
    // Elliptic curves
-   if len(j.ellipticCurves) != 0 {
-      for _, val := range j.ellipticCurves {
+   if len(supportedCurves) == 0 {
+      data = append(data, ',')
+   } else {
+      for _, val := range supportedCurves {
          data = strconv.AppendUint(data, uint64(val), 10)
          data = append(data, '-')
       }
       // Replace last dash with a comma
       data[len(data)-1] = ','
-   } else {
-      data = append(data, ',')
    }
    // ECPF
-   if len(j.ellipticCurvePF) != 0 {
-      for _, val := range j.ellipticCurvePF {
+   if len(supportedPoints) > 0 {
+      for _, val := range supportedPoints {
          data = strconv.AppendUint(data, uint64(val), 10)
          data = append(data, '-')
       }
--- research/2021-10-27/readme.md
+++ research/2021-10-27/readme.md
@@ -1,13 +1,3 @@
 # October 27 2021
 
-https://godocs.io/sort#Search
-
-`ClientHello` to `JA3`
-
-58 lines:
-
 https://github.com/open-ch/ja3/blob/d0c402d2/parser.go#L277-L335
-
-108 lines:
-
-https://github.com/dreadl0ck/ja3/blob/94ba70d7/ja3.go#L75-L183
