0329aa22921d90f9ec223453ab6a32868f3bd380
Steven Penny <srpen6@gmail.com>
2021-10-28 11:53:32 -0500

    deleted:    tls/ja3.go

--- tls/tls.go
+++ tls/tls.go
@@ -5,10 +5,13 @@ import (
    "encoding/binary"
    "fmt"
    "github.com/refraction-networking/utls"
+   "io"
    "net"
    "net/http"
    "strings"
 )
+
+const Android = "769,49195-49196-52393-49199-49200-52392-158-159-49161-49162-49171-49172-51-57-156-157-47-53,65281-0-23-35-13-16-11-10,23,0"
 
 func Handshakes(data []byte) [][]byte {
    var hands [][]byte
@@ -39,14 +42,16 @@ func Handshakes(data []byte) [][]byte {
 func NewTransport(spec *tls.ClientHelloSpec) *http.Transport {
    return &http.Transport{
       DialTLS: func(network, addr string) (net.Conn, error) {
-         dialConn, err := net.Dial(network, addr)
+         conn, err := net.Dial(network, addr)
          if err != nil {
             return nil, err
          }
-         config := &tls.Config{
-            ServerName: strings.Split(addr, ":")[0],
+         host, _, err := net.SplitHostPort(addr)
+         if err != nil {
+            return nil, err
          }
-         uconn := tls.UClient(dialConn, config, tls.HelloCustom)
+         config := &tls.Config{ServerName: host}
+         uconn := tls.UClient(conn, config, tls.HelloCustom)
          if err := uconn.ApplyPreset(spec); err != nil {
             return nil, err
          }
@@ -58,12 +63,20 @@ func NewTransport(spec *tls.ClientHelloSpec) *http.Transport {
    }
 }
 
+func extensionType(ext tls.TLSExtension) (uint16, error) {
+   data, err := io.ReadAll(ext)
+   if err != nil {
+      return 0, err
+   }
+   return binary.BigEndian.Uint16(data), nil
+}
+
 type ClientHello struct {
    *tls.ClientHelloSpec
    Version uint16
 }
 
-func NewClientHello(data []byte) (*ClientHello, error) {
+func ParseHandshake(data []byte) (*ClientHello, error) {
    if len(data) < 3 {
       return nil, fmt.Errorf("%#v", data)
    }
@@ -74,4 +87,124 @@ func NewClientHello(data []byte) (*ClientHello, error) {
       return nil, err
    }
    return &ClientHello{spec, version}, nil
+}
+
+func ParseJA3(str string) (*ClientHello, error) {
+   tokens := strings.Split(str, ",")
+   if len(tokens) != 5 {
+      return nil, fmt.Errorf("%q", tokens)
+   }
+   var version uint16
+   fmt.Sscan(tokens[0], &version)
+   hello := ClientHello{
+      new(tls.ClientHelloSpec), version,
+   }
+   // build CipherSuites
+   cipherKeys := strings.Split(tokens[1], "-")
+   for _, cipherKey := range cipherKeys {
+      var cipher uint16
+      fmt.Sscan(cipherKey, &cipher)
+      hello.CipherSuites = append(hello.CipherSuites, cipher)
+   }
+   // build extenions list
+   extKeys := strings.Split(tokens[2], "-")
+   for _, extKey := range extKeys {
+      var ext tls.TLSExtension
+      switch extKey {
+      case "0":
+         ext = &tls.SNIExtension{}
+      case "10":
+         var curves []tls.CurveID
+         curveKeys := strings.Split(tokens[3], "-")
+         for _, curveKey := range curveKeys {
+            var curve tls.CurveID
+            fmt.Sscan(curveKey, &curve)
+            curves = append(curves, curve)
+         }
+         ext = &tls.SupportedCurvesExtension{curves}
+      case "11":
+         var points []uint8
+         pointKeys := strings.Split(tokens[4], "-")
+         for _, pointKey := range pointKeys {
+            var point uint8
+            fmt.Sscan(pointKey, &point)
+            points = append(points, point)
+         }
+         ext = &tls.SupportedPointsExtension{points}
+      case "13":
+         // this cant be empty, so just use the Go default
+         ext = &tls.SignatureAlgorithmsExtension{
+            []tls.SignatureScheme{
+               0x804, 0x403, 0x807, 0x805, 0x806, 0x401,
+               0x501, 0x601, 0x503, 0x603, 0x201, 0x203,
+            },
+         }
+      case "16":
+         // if we leave this empty, it will fail on any HTTP/2 servers
+         ext = &tls.ALPNExtension{
+            []string{"http/1.1"},
+         }
+      case "23":
+         ext = &tls.UtlsExtendedMasterSecretExtension{}
+      case "35":
+         ext = &tls.SessionTicketExtension{}
+      case "65281":
+         ext = &tls.RenegotiationInfoExtension{}
+      }
+      hello.Extensions = append(hello.Extensions, ext)
+   }
+   return &hello, nil
+}
+
+func (h ClientHello) FormatJA3() (string, error) {
+   buf := new(strings.Builder)
+   // Version
+   fmt.Fprint(buf, h.Version)
+   // Cipher Suites
+   buf.WriteByte(',')
+   for key, val := range h.CipherSuites {
+      if key > 0 {
+         buf.WriteByte('-')
+      }
+      fmt.Fprint(buf, val)
+   }
+   // Extensions
+   buf.WriteByte(',')
+   var (
+      curves []tls.CurveID
+      points []uint8
+   )
+   for key, val := range h.Extensions {
+      if key > 0 {
+         buf.WriteByte('-')
+      }
+      typ, err := extensionType(val)
+      if err != nil {
+         return "", err
+      }
+      fmt.Fprint(buf, typ)
+      switch ext := val.(type) {
+      case *tls.SupportedCurvesExtension:
+         curves = ext.Curves
+      case *tls.SupportedPointsExtension:
+         points = ext.SupportedPoints
+      }
+   }
+   // Elliptic curves
+   buf.WriteByte(',')
+   for key, val := range curves {
+      if key > 0 {
+         buf.WriteByte('-')
+      }
+      fmt.Fprint(buf, val)
+   }
+   // ECPF
+   buf.WriteByte(',')
+   for key, val := range points {
+      if key > 0 {
+         buf.WriteByte('-')
+      }
+      fmt.Fprint(buf, val)
+   }
+   return buf.String(), nil
 }
--- tls/tls_test.go
+++ tls/tls_test.go
@@ -9,28 +9,41 @@ import (
 
 const android = "16030100bb010000b703034420d198e7852decbc117dc7f90550b98f2d643c954bf3361ddaf127ff921b04000024c02bc02ccca9c02fc030cca8009e009fc009c00ac013c01400330039009c009d002f00350100006aff0100010000000022002000001d636c69656e7473657276696365732e676f6f676c65617069732e636f6d0017000000230000000d0016001406010603050105030401040303010303020102030010000b000908687474702f312e31000b00020100000a000400020017"
 
-func TestHello(t *testing.T) {
-   data, err := hex.DecodeString(android)
-   if err != nil {
-      t.Fatal(err)
-   }
-   hello, err := NewClientHello(data)
-   if err != nil {
-      t.Fatal(err)
-   }
-   fmt.Printf("%+v\n", hello.ClientHelloSpec)
-   fmt.Printf("%#v\n", hello.Version)
-}
-
-func TestPcap(t *testing.T) {
+func TestHandshakes(t *testing.T) {
    data, err := os.ReadFile("PCAPdroid_25_Oct_21_53_41.pcap")
    if err != nil {
       t.Fatal(err)
    }
    for _, hand := range Handshakes(data) {
-      hello, err := NewClientHello(hand)
+      hello, err := ParseHandshake(hand)
       if err == nil {
          fmt.Printf("%+v\n", hello)
       }
+   }
+}
+
+func TestHandshake(t *testing.T) {
+   data, err := hex.DecodeString(android)
+   if err != nil {
+      t.Fatal(err)
+   }
+   hello, err := ParseHandshake(data)
+   if err != nil {
+      t.Fatal(err)
+   }
+   fmt.Printf("%+v\n", hello)
+}
+
+func TestJA3(t *testing.T) {
+   h, err := ParseJA3(Android)
+   if err != nil {
+      t.Fatal(err)
+   }
+   j, err := h.FormatJA3()
+   if err != nil {
+      t.Fatal(err)
+   }
+   if j != Android {
+      t.Fatal(j)
    }
 }
