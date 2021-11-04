98f7fec1a7551165c71f30440f3f7e2e8fcce4da
Steven Penny <srpen6@gmail.com>
2021-11-04 17:11:26 -0500

    +func Indent(v interface{}) ([]byte, error) {

--- protobuf/protobuf.go
+++ protobuf/protobuf.go
@@ -1,26 +1,33 @@
 package protobuf
 
 import (
+   "encoding/json"
    "google.golang.org/protobuf/encoding/protowire"
 )
 
-func consume(n protowire.Number, t protowire.Type, data []byte) (interface{}, int) {
+// Its kind of silly to include a one line function, but I keep forgetting how
+// to indent the result of `Parse`.
+func Indent(v interface{}) ([]byte, error) {
+   return json.MarshalIndent(v, "", " ")
+}
+
+func consume(n protowire.Number, t protowire.Type, buf []byte) (interface{}, int) {
    switch t {
    case protowire.VarintType:
-      return protowire.ConsumeVarint(data)
+      return protowire.ConsumeVarint(buf)
    case protowire.Fixed32Type:
-      return protowire.ConsumeFixed32(data)
+      return protowire.ConsumeFixed32(buf)
    case protowire.Fixed64Type:
-      return protowire.ConsumeFixed64(data)
+      return protowire.ConsumeFixed64(buf)
    case protowire.BytesType:
-      v, vLen := protowire.ConsumeBytes(data)
+      v, vLen := protowire.ConsumeBytes(buf)
       sub := Parse(v)
       if sub != nil {
          return sub, vLen
       }
       return string(v), vLen
    case protowire.StartGroupType:
-      v, vLen := protowire.ConsumeGroup(n, data)
+      v, vLen := protowire.ConsumeGroup(n, buf)
       sub := Parse(v)
       if sub != nil {
          return sub, vLen
@@ -36,23 +43,24 @@ type Field struct {
    Value interface{}
 }
 
-func Parse(data []byte) []Field {
-   var flds []Field
-   for len(data) > 0 {
-      n, t, fLen := protowire.ConsumeField(data)
+func Parse(buf []byte) []Field {
+   var fields []Field
+   for len(buf) > 0 {
+      n, t, fLen := protowire.ConsumeField(buf)
       if fLen <= 0 {
          return nil
       }
-      _, _, tLen := protowire.ConsumeTag(data[:fLen])
+      _, _, tLen := protowire.ConsumeTag(buf[:fLen])
       if tLen <= 0 {
          return nil
       }
-      v, vLen := consume(n, t, data[tLen:fLen])
+      v, vLen := consume(n, t, buf[tLen:fLen])
       if vLen <= 0 {
          return nil
       }
-      flds = append(flds, Field{n, t, v})
-      data = data[fLen:]
+      fields = append(fields, Field{n, t, v})
+      buf = buf[fLen:]
    }
-   return flds
+   return fields
 }
+
--- protobuf/protobuf_test.go
+++ protobuf/protobuf_test.go
@@ -1,16 +1,17 @@
 package protobuf
 
 import (
-   "fmt"
    "os"
    "testing"
 )
 
+var buf = []byte("\nU\xe2\x01R\nPCjkaNwoTMzk0NzAwOTc1MjU3MDUyNzQ0MBIgChAxNjM2MDYyNjA5NzAzMjkxEgwIkauRjAYQ-LytzwI=*\x03\b\xf3\x01J\x1c\b\x12\x9a\x01\x17\n\x13\b\x84\xb3\xad\x95\xd8\xff\xf3\x02\x15a\x05\xc9\n\x1dz7\x05\xd4\x10\x01")
+
 func TestProto(t *testing.T) {
-   data, err := os.ReadFile("details.txt")
+   fields := Parse(buf)
+   buf, err := Indent(fields)
    if err != nil {
       t.Fatal(err)
    }
-   toks := Parse(data)
-   fmt.Printf("%+v\n", toks)
+   os.Stdout.Write(buf)
 }
