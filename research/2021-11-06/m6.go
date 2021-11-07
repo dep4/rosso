bc97a3bdb0a80acb73d3dfe23824c3ee7b872c97
Steven Penny <srpen6@gmail.com>
2021-11-06 20:04:52 -0500

    new file:   research/2021-11-06/m7.go

--- research/2021-11-06/m6.go
+++ research/2021-11-06/m6.go
@@ -2,25 +2,56 @@ package main
 
 import (
    "fmt"
+   "github.com/segmentio/encoding/proto"
    "google.golang.org/protobuf/encoding/protowire"
 )
+
+type Object struct {
+   Array []string `protobuf:"bytes,10"`
+}
 
 type (
    array []interface{}
    object map[protowire.Number]interface{}
 )
 
-var defaultConfig = object{
-   10: array{
-      "android.hardware.camera",
-      "android.hardware.faketouch",
-      "android.hardware.location",
-      "android.hardware.screen.portrait",
-      "android.hardware.touchscreen",
-      "android.hardware.wifi",
-   },
+func appendField(out []byte, key protowire.Number, val interface{}) []byte {
+   switch val := val.(type) {
+   case string:
+      out = protowire.AppendTag(out, key, protowire.BytesType)
+      out = protowire.AppendString(out, val)
+   case array:
+      for _, v := range val {
+         out = appendField(out, key, v)
+      }
+   }
+   return out
+}
+
+func (o object) marshal() []byte {
+   var out []byte
+   for key, val := range o {
+      out = appendField(out, key, val)
+   }
+   return out
 }
 
 func main() {
-   fmt.Println(defaultConfig)
+   {
+      obj := object{
+         10: array{"hello", "world"},
+      }
+      buf := obj.marshal()
+      fmt.Printf("%q\n", buf)
+   }
+   {
+      obj := Object{
+         Array: []string{"hello", "world"},
+      }
+      buf, err := proto.Marshal(obj)
+      if err != nil {
+         panic(err)
+      }
+      fmt.Printf("%q\n", buf)
+   }
 }
