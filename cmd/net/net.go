4ff3a5f9ee4ff8bbeb2317a37b544591835320fe
Steven Penny <srpen6@gmail.com>
2021-11-16 00:03:47 -0600

    +      dec := protobuf.NewDecoder(buf)

--- cmd/net/net.go
+++ cmd/net/net.go
@@ -1,34 +1,63 @@
 package main
 
 import (
+   "encoding/json"
    "flag"
    "fmt"
    "github.com/89z/parse/net"
+   "github.com/89z/parse/protobuf"
+   "io"
    "net/http"
    "net/http/httputil"
    "os"
 )
 
+func encode(r io.Reader, w io.Writer, proto bool) error {
+   if proto {
+      buf, err := io.ReadAll(r)
+      if err != nil {
+         return err
+      }
+      dec := protobuf.NewDecoder(buf)
+      enc := json.NewEncoder(w)
+      enc.SetIndent("", " ")
+      return enc.Encode(dec)
+   }
+   _, err := io.Copy(w, r)
+   if err != nil {
+      return err
+   }
+   return nil
+}
+
+func file(output string) (*os.File, error) {
+   if output != "" {
+      return os.Create(output)
+   }
+   return os.Stdout, nil
+}
+
 func main() {
    var (
-      https bool
+      https, proto bool
       output string
    )
    flag.BoolVar(&https, "s", false, "HTTPS")
+   flag.BoolVar(&proto, "p", false, "Protocol Buffer")
    flag.StringVar(&output, "o", "", "output file")
    flag.Parse()
    if flag.NArg() != 1 {
-      fmt.Println("http [flags] [request file]")
+      fmt.Println("net [flags] [request file]")
       flag.PrintDefaults()
       return
    }
-   file := flag.Arg(0)
-   rd, err := os.Open(file)
+   input := flag.Arg(0)
+   read, err := os.Open(input)
    if err != nil {
       panic(err)
    }
-   defer rd.Close()
-   req, err := net.ReadRequest(rd)
+   defer read.Close()
+   req, err := net.ReadRequest(read)
    if err != nil {
       panic(err)
    }
@@ -42,19 +71,19 @@ func main() {
       panic(err)
    }
    defer res.Body.Close()
+   // head
    buf, err := httputil.DumpResponse(res, false)
    if err != nil {
       panic(err)
    }
    os.Stdout.Write(buf)
-   if output == "" {
-      os.Stdout.ReadFrom(res.Body)
-      return
-   }
-   wr, err := os.Create(output)
+   // body
+   write, err := file(output)
    if err != nil {
       panic(err)
    }
-   defer wr.Close()
-   wr.ReadFrom(res.Body)
+   defer write.Close()
+   if err := encode(res.Body, write, proto); err != nil {
+      panic(err)
+   }
 }
