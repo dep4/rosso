9d88effb69a84fb82b3e565b9778aecf2fbe826c
Steven Penny <srpen6@gmail.com>
2021-12-14 07:42:12 -0600

    +   flag.BoolVar(&info, "i", false, "info")

--- crypto/binary.go
+++ crypto/binary.go
@@ -2,9 +2,25 @@ package crypto
 
 import (
    "encoding/binary"
+   "github.com/89z/parse"
    "github.com/refraction-networking/utls"
    "io"
 )
+
+func ParseHandshake(data []byte) (*ClientHello, error) {
+   // Content Type, Version
+   if dLen := len(data); dLen <= 2 {
+      return nil, parse.InvalidSlice{2, dLen}
+   }
+   version := binary.BigEndian.Uint16(data[1:])
+   // unsupported extension 0x16
+   fin := tls.Fingerprinter{AllowBluntMimicry: true}
+   spec, err := fin.FingerprintClientHello(data)
+   if err != nil {
+      return nil, err
+   }
+   return &ClientHello{spec, version}, nil
+}
 
 func (b *Buffer) ReadUint16LengthPrefixed() ([]byte, []byte, bool) {
    low := 2
@@ -41,15 +57,4 @@ func extensionType(ext tls.TLSExtension) (uint16, error) {
       return 0, err
    }
    return binary.BigEndian.Uint16(data), nil
-}
-
-func ParseHandshake(data []byte) (*ClientHello, error) {
-   // unsupported extension 0x16
-   fin := tls.Fingerprinter{AllowBluntMimicry: true}
-   spec, err := fin.FingerprintClientHello(data)
-   if err != nil {
-      return nil, err
-   }
-   version := binary.BigEndian.Uint16(data[1:])
-   return &ClientHello{spec, version}, nil
 }
