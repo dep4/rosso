5c38228c33596f5f60b8d3e5d15d8933906e09d5
Steven Penny <srpen6@gmail.com>
2021-12-16 07:28:38 -0600

    +      req.ContentLength = length

--- net/net.go
+++ net/net.go
@@ -6,34 +6,49 @@ import (
    "net/http"
    "net/textproto"
    "net/url"
+   "strconv"
    "strings"
 )
 
 func ReadRequest(src io.Reader) (*http.Request, error) {
+   var req http.Request
    text := textproto.NewReader(bufio.NewReader(src))
-   line, err := text.ReadLine()
-   if err != nil {
-      return nil, err
-   }
-   head, err := text.ReadMIMEHeader()
+   // .Method
+   sMethodPath, err := text.ReadLine()
    if err != nil {
       return nil, err
    }
    // GET /fdfe/details?doc=com.instagram.android HTTP/1.1
-   methodURL := strings.Fields(line)
-   if len(methodURL) != 3 {
-      return nil, textproto.ProtocolError(line)
+   methodPath := strings.Fields(sMethodPath)
+   if len(methodPath) != 3 {
+      return nil, textproto.ProtocolError(sMethodPath)
    }
-   addr, err := url.Parse(methodURL[1])
+   req.Method = methodPath[0]
+   // .URL
+   addr, err := url.Parse(methodPath[1])
    if err != nil {
       return nil, err
    }
-   addr.Host = head.Get("Host")
-   var req http.Request
-   req.Body = io.NopCloser(text.R)
-   req.Header = http.Header(head)
-   req.Method = methodURL[0]
    req.URL = addr
+   // .URL.Host
+   head, err := text.ReadMIMEHeader()
+   if err != nil {
+      return nil, err
+   }
+   req.URL.Host = head.Get("Host")
+   // .Header
+   req.Header = http.Header(head)
+   // .ContentLength
+   sLength := head.Get("Content-Length")
+   if sLength != "" {
+      length, err := strconv.ParseInt(sLength, 10, 64)
+      if err != nil {
+         return nil, err
+      }
+      req.ContentLength = length
+   }
+   // .Body
+   req.Body = io.NopCloser(text.R)
    return &req, nil
 }
 
