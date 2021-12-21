6f8414e80559c5d74ca441a28e13df5a70d20ae0
name <srpen6@gmail.com>
2021-12-21 17:28:31 -0600

    +      req.ContentLength = length

--- net/net_test.go
+++ net/net_test.go
@@ -7,21 +7,25 @@ import (
    "testing"
 )
 
-const details = `GET /fdfe/details?doc=com.instagram.android HTTP/1.1
-Host: android.clients.google.com
-Authorization: Bearer ya29.a0ARrdaM9rMbLUSP6wDIQGuLUH7Ej7vodCwekHOJx8J_JRD2k1...
-X-DFE-Device-ID: 3a1c36f387b...
+const post = `POST /api/v1/playlist/getFragment HTTP/1.1
+Host: pandora.com
+content-type:application/json
+cookie:csrftoken=842b12c83a3c5153
+x-authtoken:BXoTKywEhnoiEqDEcu0U/qGlFBEK5Tjblz3fgnLPgFojficRTR8Xm6Lw==
+x-csrftoken:842b12c83a3c5153
+content-length: 54
 
-`
+{"isStationStart":true,"stationId":126608766085892525}`
 
 func TestRequest(t *testing.T) {
-   req, err := ReadRequest(strings.NewReader(details))
+   req, err := readRequest(strings.NewReader(post))
    if err != nil {
       t.Fatal(err)
    }
-   buf, err := httputil.DumpRequest(req, false)
+   req.URL.Scheme = "http"
+   buf, err := httputil.DumpRequestOut(req, true)
    if err != nil {
       t.Fatal(err)
    }
-   os.Stdout.Write(buf)
+   os.Stdout.Write(append(buf, '\n'))
 }
