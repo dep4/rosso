0f78bbb2ef2667fc3ba894c106ac1041b5e68a8d
name <srpen6@gmail.com>
2021-12-21 17:28:31 -0600

    new file:   research/2021-12-17/readme.md

--- net/net.go
+++ net/net.go
@@ -51,17 +51,3 @@ func ReadRequest(src io.Reader) (*http.Request, error) {
    req.Body = io.NopCloser(text.R)
    return &req, nil
 }
-
-// text/plain encoding algorithm
-// html.spec.whatwg.org/multipage/form-control-infrastructure.html
-func ReadQuery(src io.Reader) url.Values {
-   vals := make(url.Values)
-   buf := bufio.NewScanner(src)
-   for buf.Scan() {
-      kv := strings.SplitN(buf.Text(), "=", 2)
-      if len(kv) == 2 {
-         vals.Add(kv[0], kv[1])
-      }
-   }
-   return vals
-}
--- net/readme.md
+++
@@ -1,4 +0,0 @@
-# Net
-
-- https://github.com/ytdl-org/youtube-dl/issues/30250
-- https://podcasts.apple.com/us/podcast/the-joy-of-basketball-cookies-hoops-x-pdl/id1471240409?i=1000542112826
