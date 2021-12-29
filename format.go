f03cb71a735b9cbc96e65bc22cce89debbcce41d
name <srpen6@gmail.com>
2021-12-29 07:54:09 -0600

    renamed:    research/2021-12-25/readme.md -> research/2021-12-28/readme.md

--- format.go
+++ format.go
@@ -1,7 +1,13 @@
 package format
 
 import (
+   "bytes"
+   "fmt"
+   "net/http"
+   "net/http/httputil"
+   "os"
    "strconv"
+   "time"
 )
 
 var (
@@ -31,12 +37,90 @@ func PercentInt64(value, total int64) string {
    return Percent(val, tot)
 }
 
-// github.com/golang/text/blob/18b340fc/encoding/internal/enctest/enctest.go#L175-L180
 func Trim(s string) string {
    if len(s) <= 99 {
       return s
    }
    return s[:48] + "..." + s[len(s)-48:]
+}
+
+type LogLevel int
+
+func (l LogLevel) Dump(req *http.Request) error {
+   switch l {
+   case 0:
+      loc := Trim(req.URL.String())
+      fmt.Println(req.Method, loc)
+   case 1:
+      buf, err := httputil.DumpRequest(req, true)
+      if err != nil {
+         return err
+      }
+      os.Stdout.Write(buf)
+      if !bytes.HasSuffix(buf, []byte{'\n'}) {
+         os.Stdout.WriteString("\n")
+      }
+   case 2:
+      buf, err := httputil.DumpRequestOut(req, true)
+      if err != nil {
+         return err
+      }
+      os.Stdout.Write(buf)
+   }
+   return nil
+}
+
+type Progress struct {
+   *http.Response
+   Content, PartLength, part int64
+   time.Time
+}
+
+func Content(length int64) Progress {
+   pro := Progress{PartLength: 10_000_000}
+   pro.ContentLength = length
+   pro.Time = time.Now()
+   return pro
+}
+
+func Response(res *http.Response) *Progress {
+   pro := Progress{Response: res, PartLength: 10_000_000}
+   pro.Time = time.Now()
+   return &pro
+}
+
+func (p Progress) Print() {
+   end := time.Since(p.Time).Milliseconds()
+   if end > 0 {
+      meter := PercentInt64(p.Content, p.ContentLength)
+      meter += "\t" + Size.LabelInt(p.Content)
+      meter += "\t" + Rate.LabelInt(1000 * p.Content / end)
+      fmt.Println(meter)
+   }
+}
+
+func (p Progress) Range() string {
+   buf := []byte("bytes=")
+   buf = strconv.AppendInt(buf, p.Content, 10)
+   buf = append(buf, '-')
+   buf = strconv.AppendInt(buf, p.Content+p.PartLength-1, 10)
+   return string(buf)
+}
+
+func (p *Progress) Read(buf []byte) (int, error) {
+   if p.part == 0 {
+      p.Print()
+   }
+   read, err := p.Body.Read(buf)
+   if err != nil {
+      return 0, err
+   }
+   p.Content += int64(read)
+   p.part += int64(read)
+   if p.part >= p.PartLength {
+      p.part = 0
+   }
+   return read, nil
 }
 
 type Symbols []string
