aa5c96498659685bb935626859d7377841307ef4
Zombo <srpen6@gmail.com>
2021-12-31 00:16:03 -0600

    new file:   cmd/net/main.go

---
+++ cmd/net/main.go
@@ -0,0 +1,60 @@
+package main
+
+import (
+   "flag"
+   "fmt"
+   "github.com/89z/format/net"
+   "net/http"
+   "net/http/httputil"
+   "os"
+)
+
+func main() {
+   var (
+      https, info, proto bool
+      output string
+   )
+   flag.BoolVar(&info, "i", false, "info")
+   flag.StringVar(&output, "o", "", "output file")
+   flag.BoolVar(&proto, "p", false, "Protocol Buffer")
+   flag.BoolVar(&https, "s", false, "HTTPS")
+   flag.Parse()
+   if flag.NArg() != 1 {
+      fmt.Println("net [flags] [request file]")
+      flag.PrintDefaults()
+      return
+   }
+   input := flag.Arg(0)
+   read, err := os.Open(input)
+   if err != nil {
+      panic(err)
+   }
+   defer read.Close()
+   req, err := net.ReadRequest(read)
+   if err != nil {
+      panic(err)
+   }
+   if info {
+      fmt.Printf("%#v\n", req.URL.Query())
+      fmt.Printf("%#v\n", req.Header)
+   }
+   if https {
+      req.URL.Scheme = "https"
+   } else {
+      req.URL.Scheme = "http"
+   }
+   res, err := new(http.Transport).RoundTrip(req)
+   if err != nil {
+      panic(err)
+   }
+   defer res.Body.Close()
+   // head
+   buf, err := httputil.DumpResponse(res, false)
+   if err != nil {
+      panic(err)
+   }
+   os.Stdout.Write(buf)
+   if err := readFrom(res.Body, output, proto); err != nil {
+      panic(err)
+   }
+}
--- cmd/net/net.go
+++ cmd/net/net.go
@@ -2,90 +2,39 @@ package main
 
 import (
    "bytes"
-   "flag"
    "encoding/json"
-   "fmt"
-   "github.com/89z/format/net"
    "github.com/89z/format/protobuf"
-   "net/http"
-   "net/http/httputil"
+   "io"
    "os"
 )
 
-func file(output string) (*os.File, error) {
-   if output != "" {
-      return os.Create(output)
-   }
-   return os.Stdout, nil
-}
-
-func main() {
-   var (
-      https, info, proto bool
-      output string
-   )
-   flag.BoolVar(&info, "i", false, "info")
-   flag.StringVar(&output, "o", "", "output file")
-   flag.BoolVar(&proto, "p", false, "Protocol Buffer")
-   flag.BoolVar(&https, "s", false, "HTTPS")
-   flag.Parse()
-   if flag.NArg() != 1 {
-      fmt.Println("net [flags] [request file]")
-      flag.PrintDefaults()
-      return
-   }
-   input := flag.Arg(0)
-   read, err := os.Open(input)
+func readFrom(src io.Reader, output string, proto bool) error {
+   dst, err := os.Create(output)
    if err != nil {
-      panic(err)
+      dst = os.Stdout
    }
-   defer read.Close()
-   req, err := net.ReadRequest(read)
-   if err != nil {
-      panic(err)
-   }
-   if info {
-      fmt.Printf("%#v\n", req.URL.Query())
-      fmt.Printf("%#v\n", req.Header)
-   }
-   if https {
-      req.URL.Scheme = "https"
-   } else {
-      req.URL.Scheme = "http"
-   }
-   res, err := new(http.Transport).RoundTrip(req)
-   if err != nil {
-      panic(err)
-   }
-   defer res.Body.Close()
-   // head
-   buf, err := httputil.DumpResponse(res, false)
-   if err != nil {
-      panic(err)
-   }
-   os.Stdout.Write(buf)
-   // body
-   write, err := file(output)
-   if err != nil {
-      panic(err)
-   }
-   defer write.Close()
+   defer dst.Close()
    if proto {
-      mes, err := protobuf.Decode(res.Body)
+      mes, err := protobuf.Decode(src)
       if err != nil {
-         panic(err)
+         return err
       }
       buf, err := json.Marshal(mes)
       if err != nil {
-         panic(err)
+         return err
       }
-      if _, err := write.ReadFrom(bytes.NewReader(buf)); err != nil {
-         panic(err)
+      indent := new(bytes.Buffer)
+      if err := json.Indent(indent, buf, "", " "); err != nil {
+         return err
+      }
+      if _, err := dst.ReadFrom(indent); err != nil {
+         return err
       }
    } else {
-      _, err := write.ReadFrom(res.Body)
+      _, err := dst.ReadFrom(src)
       if err != nil {
-         panic(err)
+         return err
       }
    }
+   return nil
 }
