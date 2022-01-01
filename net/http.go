712a37f0f08d5bb88c148e829e00202e71e12575
Zombo <srpen6@gmail.com>
2022-01-01 10:28:11 -0600

    +   pro.Response = &http.Response{ContentLength: length}

--- net/http.go
+++ net/http.go
@@ -1,16 +1,19 @@
 package net
 
 import (
+   "bytes"
    "bufio"
+   "fmt"
    "io"
    "net/http"
    "net/textproto"
    "net/url"
    "strconv"
    "strings"
+   "time"
 )
 
-func ReadRequest(src io.Reader) (*http.Request, error) {
+func ReadRequest(src io.Reader, https bool) (*http.Request, error) {
    var req http.Request
    text := textproto.NewReader(bufio.NewReader(src))
    // .Method
@@ -18,7 +21,6 @@ func ReadRequest(src io.Reader) (*http.Request, error) {
    if err != nil {
       return nil, err
    }
-   // GET /fdfe/details?doc=com.instagram.android HTTP/1.1
    methodPath := strings.Fields(sMethodPath)
    if len(methodPath) != 3 {
       return nil, textproto.ProtocolError(sMethodPath)
@@ -30,6 +32,12 @@ func ReadRequest(src io.Reader) (*http.Request, error) {
       return nil, err
    }
    req.URL = addr
+   // .URL.Scheme
+   if https {
+      req.URL.Scheme = "https"
+   } else {
+      req.URL.Scheme = "http"
+   }
    // .URL.Host
    head, err := text.ReadMIMEHeader()
    if err != nil {
@@ -50,4 +58,29 @@ func ReadRequest(src io.Reader) (*http.Request, error) {
    // .Body
    req.Body = io.NopCloser(text.R)
    return &req, nil
+}
+
+func WriteRequest(dst io.Writer, req *http.Request) error {
+   now := strconv.FormatInt(time.Now().UnixMicro(), 36)
+   if req.Body != nil && req.Method == "POST" {
+      buf, err := io.ReadAll(req.Body)
+      if err != nil {
+         return err
+      }
+      req.Body = io.NopCloser(bytes.NewReader(buf))
+      fmt.Fprintf(dst, "var b%v = strings.NewReader(%q)\n", now, buf)
+   }
+   fmt.Fprintf(dst, "var r%v = &http.Request{", now)
+   // .Method
+   fmt.Fprintf(dst, "Method:%q", req.Method)
+   // .URL
+   fmt.Fprintf(dst, ", URL:%#v", req.URL)
+   // .Header
+   fmt.Fprintf(dst, ", Header:%#v", req.Header)
+   // .Body
+   if req.Body != nil && req.Method == "POST" {
+      fmt.Fprintf(dst, ", Body:io.NopCloser(b%v)", now)
+   }
+   fmt.Fprintln(dst, "}")
+   return nil
 }
