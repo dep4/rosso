196bb4e4fbe8024f1be09790783c01ccb88097c0
Zombo <srpen6@gmail.com>
2022-01-03 17:08:43 -0600

    +      bProto, err := io.ReadAll(src)

--- protobuf/values.go
+++ protobuf/values.go
@@ -6,65 +6,114 @@ import (
 
 func consumeBytes(b []byte) ([]byte, error) {
    val, vLen := protowire.ConsumeBytes(b)
-   err := protowire.ParseError(vLen)
-   if err != nil {
-      return nil, err
-   }
-   return val, nil
+   return val, protowire.ParseError(vLen)
 }
 
 func consumeField(b []byte) (protowire.Number, protowire.Type, int, error) {
    num, typ, fLen := protowire.ConsumeField(b)
-   err := protowire.ParseError(fLen)
-   if err != nil {
-      return 0, 0, 0, err
-   }
-   return num, typ, fLen, nil
+   return num, typ, fLen, protowire.ParseError(fLen)
 }
 
 func consumeFixed32(b []byte) (uint32, error) {
    val, vLen := protowire.ConsumeFixed32(b)
-   err := protowire.ParseError(vLen)
-   if err != nil {
-      return 0, err
-   }
-   return val, nil
+   return val, protowire.ParseError(vLen)
 }
 
 func consumeFixed64(b []byte) (uint64, error) {
    val, vLen := protowire.ConsumeFixed64(b)
-   err := protowire.ParseError(vLen)
-   if err != nil {
-      return 0, err
-   }
-   return val, nil
+   return val, protowire.ParseError(vLen)
 }
 
 func consumeGroup(num protowire.Number, b []byte) ([]byte, error) {
    val, vLen := protowire.ConsumeGroup(num, b)
-   err := protowire.ParseError(vLen)
-   if err != nil {
-      return nil, err
-   }
-   return val, nil
+   return val, protowire.ParseError(vLen)
 }
 
 func consumeTag(b []byte) (int, error) {
    _, _, tLen := protowire.ConsumeTag(b)
-   err := protowire.ParseError(tLen)
-   if err != nil {
-      return 0, err
-   }
-   return tLen, nil
+   return tLen, protowire.ParseError(tLen)
 }
 
 func consumeVarint(b []byte) (uint64, error) {
    val, vLen := protowire.ConsumeVarint(b)
-   err := protowire.ParseError(vLen)
-   if err != nil {
-      return 0, err
+   return val, protowire.ParseError(vLen)
+}
+
+func (m Message) Add(num protowire.Number, str string, val Message) {
+   tag := Tag{num, str}
+   switch typ := m[tag].(type) {
+   case nil:
+      m[tag] = val
+   case Message:
+      m[tag] = []Message{typ, val}
+   case []Message:
+      m[tag] = append(typ, val)
    }
-   return val, nil
+}
+
+func (m Message) Get(num protowire.Number, str string) Message {
+   val, ok := m[Tag{num, str}].(Message)
+   if ok {
+      return val
+   } else {
+      val, ok := m[Tag{num, ""}].(Message)
+      if ok {
+         return val
+      }
+   }
+   return nil
+}
+
+func (m Message) GetBytes(num protowire.Number, str string) []byte {
+   val, ok := m[Tag{num, str}].([]byte)
+   if ok {
+      return val
+   } else {
+      val, ok := m[Tag{num, ""}].([]byte)
+      if ok {
+         return val
+      }
+   }
+   return nil
+}
+
+func (m Message) GetMessages(num protowire.Number, str string) []Message {
+   val, ok := m[Tag{num, str}].([]Message)
+   if ok {
+      return val
+   } else {
+      val, ok := m[Tag{num, ""}].([]Message)
+      if ok {
+         return val
+      }
+   }
+   return nil
+}
+
+func (m Message) GetString(num protowire.Number, str string) string {
+   val, ok := m[Tag{num, str}].(string)
+   if ok {
+      return val
+   } else {
+      val, ok := m[Tag{num, ""}].(string)
+      if ok {
+         return val
+      }
+   }
+   return ""
+}
+
+func (m Message) GetUint(num protowire.Number, str string) uint64 {
+   val, ok := m[Tag{num, str}].(uint64)
+   if ok {
+      return val
+   } else {
+      val, ok := m[Tag{num, ""}].(uint64)
+      if ok {
+         return val
+      }
+   }
+   return 0
 }
 
 func (m Message) addBytes(key protowire.Number, val []byte) {
@@ -114,91 +163,3 @@ func (m Message) addUint64(key protowire.Number, val uint64) {
       m[tag] = append(typ, val)
    }
 }
-
-////////////////////////////////////////////////////////////////////////////////
-
-func (m Message) Add(key protowire.Number, val Message) {
-   tag := Tag{Number: key}
-   switch typ := m[tag].(type) {
-   case nil:
-      m[tag] = val
-   case Message:
-      m[tag] = []Message{typ, val}
-   case []Message:
-      m[tag] = append(typ, val)
-   }
-}
-
-func (m Message) Get(keys ...protowire.Number) Message {
-   for _, key := range keys {
-      tag := Tag{Number: key}
-      val, ok := m[tag].(Message)
-      if ok {
-         m = val
-      }
-   }
-   return m
-}
-
-func (m Message) GetBytes(keys ...protowire.Number) []byte {
-   for _, key := range keys {
-      tag := Tag{Number: key}
-      switch val := m[tag].(type) {
-      case Message:
-         m = val
-      case []byte:
-         return val
-      }
-   }
-   return nil
-}
-
-func (m Message) GetMessages(keys ...protowire.Number) []Message {
-   for _, key := range keys {
-      tag := Tag{Number: key}
-      switch val := m[tag].(type) {
-      case Message:
-         m = val
-      case []Message:
-         return val
-      }
-   }
-   return nil
-}
-
-func (m Message) GetString(keys ...protowire.Number) string {
-   for _, key := range keys {
-      tag := Tag{Number: key}
-      switch val := m[tag].(type) {
-      case Message:
-         m = val
-      case string:
-         return val
-      }
-   }
-   return ""
-}
-
-func (m Message) GetUint64(keys ...protowire.Number) uint64 {
-   for _, key := range keys {
-      tag := Tag{Number: key}
-      switch val := m[tag].(type) {
-      case Message:
-         m = val
-      case uint64:
-         return val
-      }
-   }
-   return 0
-}
-
-func (m Message) Set(key protowire.Number, val Message) {
-   tag := Tag{Number: key}
-   m[tag] = val
-}
-
-func (m Message) SetUint64(key protowire.Number, val uint64) {
-   tag := Tag{Number: key}
-   m[tag] = val
-}
-
