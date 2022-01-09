044259dcb196d0239040cc5d504dda97148850b4
Zombo <srpen6@gmail.com>
2022-01-09 08:07:44 -0600

    deleted:    research/2022-01-08/body.go

--- crypto/parse_test.go
+++ crypto/parse_test.go
@@ -1,6 +1,7 @@
 package crypto
 
 import (
+   "encoding/hex"
    "fmt"
    "net/http"
    "net/url"
@@ -9,9 +10,34 @@ import (
    "time"
 )
 
+const androidHandshake =
+   "16030100bb010000b703034420d198e7852decbc117dc7f90550b98f2d643c954bf3361d" +
+   "daf127ff921b04000024c02bc02ccca9c02fc030cca8009e009fc009c00ac013c0140033" +
+   "0039009c009d002f00350100006aff0100010000000022002000001d636c69656e747365" +
+   "7276696365732e676f6f676c65617069732e636f6d0017000000230000000d0016001406" +
+   "010603050105030401040303010303020102030010000b000908687474702f312e31000b" +
+   "00020100000a000400020017"
+
+const curlHandshake =
+   "1603010200010001fc03033356ee099c006213ecb9f7493ef981dd513761eae27eff36a1" +
+   "77ebd353fc207520fa9ef53871b81af022e38d46ca9268be95889d6e964db818768ec86a" +
+   "68c7216f003e130213031301c02cc030009fcca9cca8ccaac02bc02f009ec024c028006b" +
+   "c023c0270067c00ac0140039c009c0130033009d009c003d003c0035002f00ff01000175" +
+   "00000010000e00000b6578616d706c652e636f6d000b000403000102000a000c000a001d" +
+   "0017001e00190018337400000010000e000c02683208687474702f312e31001600000017" +
+   "000000310000000d0030002e040305030603080708080809080a080b0804080508060401" +
+   "05010601030302030301020103020202040205020602002b000908030403030302030100" +
+   "2d00020101003300260024001d002034107e2fb61cbfc3c827b3d574b57d9d5f5294bedb" +
+   "7ee350407c05d1a9396b5b001500b2000000000000000000000000000000000000000000" +
+   "000000000000000000000000000000000000000000000000000000000000000000000000" +
+   "000000000000000000000000000000000000000000000000000000000000000000000000" +
+   "000000000000000000000000000000000000000000000000000000000000000000000000" +
+   "000000000000000000000000000000000000000000000000000000000000000000000000" +
+   "00000000000000000000000000"
+
 var hellos = []string{AndroidAPI24, AndroidAPI25, AndroidAPI26, AndroidAPI29}
 
-func TestParse(t *testing.T) {
+func TestParseJA3(t *testing.T) {
    val := url.Values{
       "Email": {"srpen6@gmail.com"},
       "EncryptedPasswd": {passwd},
@@ -23,7 +49,8 @@ func TestParse(t *testing.T) {
          t.Fatal(err)
       }
       req, err := http.NewRequest(
-         "POST", "https://android.clients.google.com/auth", strings.NewReader(val),
+         "POST", "https://android.clients.google.com/auth",
+         strings.NewReader(val),
       )
       if err != nil {
          t.Fatal(err)
@@ -36,5 +63,24 @@ func TestParse(t *testing.T) {
       defer res.Body.Close()
       fmt.Println(res.Status, hello)
       time.Sleep(time.Second)
+   }
+}
+
+func TestParseTLS(t *testing.T) {
+   hands := []string{androidHandshake, curlHandshake}
+   for _, hand := range hands {
+      data, err := hex.DecodeString(hand)
+      if err != nil {
+         t.Fatal(err)
+      }
+      hello, err := ParseTLS(data)
+      if err != nil {
+         t.Fatal(err)
+      }
+      ja3, err := FormatJA3(hello)
+      if err != nil {
+         t.Fatal(err)
+      }
+      fmt.Println(ja3)
    }
 }
--- research/2022-01-08/body.go
+++
@@ -1,7 +0,0 @@
-package main
-
-import (
-   "strings"
-)
-
-var body0 = strings.NewReader("{\"deviceToken\":{\"deviceTypeId\":\"A1DL2DVDQVK3Q\",\"deviceId\":\"ANDROIDSDKBUILTFORX861E87F7F4978B76EE\"},\"appInfo\":{\"musicAgent\":\"Harley/3.9.1.286 Harley/17.19.4 ( fc85fac4-38d2-4c2c-b9c4-e0d0e482a71c B002OHXKZ4 )\"},\"contentIdList\":[{\"identifier\":\"B002OHXKZ4\",\"identifierType\":\"ASIN\"}],\"musicDashVersionList\":[\"SIREN_KATANA\"],\"contentProtectionList\":[\"GROUP_PSSH\"],\"customerInfo\":{\"marketplaceId\":\"ATVPDKIKX0DER\",\"territoryId\":\"US\"},\"tryAsinSubstitution\":true,\"try3dAsinSubstitution\":false}")
