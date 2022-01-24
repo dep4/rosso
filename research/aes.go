5dd35d31196fc851a6099ad3c3907c348b066a4d
Zombo <srpen6@gmail.com>
2022-01-23 21:01:11 -0600

    +func pkcs5UnPadding(origData []byte) []byte {

--- research/aes.go
+++ research/aes.go
@@ -17,6 +17,87 @@ import (
    "sync/atomic"
 )
 
+func pkcs5UnPadding(origData []byte) []byte {
+   length := len(origData)
+   unPadding := int(origData[length-1])
+   return origData[:(length - unPadding)]
+}
+
+func AES128Decrypt(crypted, key, iv []byte) ([]byte, error) {
+   block, err := aes.NewCipher(key)
+   if err != nil {
+      return nil, err
+   }
+   blockSize := block.BlockSize()
+   if len(iv) == 0 {
+      iv = key
+   }
+   blockMode := cipher.NewCBCDecrypter(block, iv[:blockSize])
+   origData := make([]byte, len(crypted))
+   blockMode.CryptBlocks(origData, crypted)
+   origData = pkcs5UnPadding(origData)
+   return origData, nil
+}
+
+func (d *Downloader) download(segIndex int) error {
+   tsFilename := tsFilename(segIndex)
+   tsUrl := d.tsURL(segIndex)
+   b, e := Get(tsUrl)
+   if e != nil {
+   return fmt.Errorf("request %s, %s", tsUrl, e.Error())
+   }
+   //noinspection GoUnhandledErrorResult
+   defer b.Close()
+   fPath := filepath.Join(d.tsFolder, tsFilename)
+   fTemp := fPath + tsTempFileSuffix
+   f, err := os.Create(fTemp)
+   if err != nil {
+   return fmt.Errorf("create file: %s, %s", tsFilename, err.Error())
+   }
+   bytes, err := ioutil.ReadAll(b)
+   if err != nil {
+   return fmt.Errorf("read bytes: %s, %s", tsUrl, err.Error())
+   }
+   sf := d.result.M3u8.Segments[segIndex]
+   if sf == nil {
+   return fmt.Errorf("invalid segment index: %d", segIndex)
+   }
+   key, ok := d.result.Keys[sf.KeyIndex]
+   if ok && key != "" {
+   bytes, err = AES128Decrypt(bytes, []byte(key),
+   []byte(d.result.M3u8.Keys[sf.KeyIndex].IV))
+   if err != nil {
+   return fmt.Errorf("decryt: %s, %s", tsUrl, err.Error())
+   }
+   }
+   // https://en.wikipedia.org/wiki/MPEG_transport_stream
+   // Some TS files do not start with SyncByte 0x47, they can not be played
+   // after merging, Need to remove the bytes before the SyncByte 0x47(71).
+   syncByte := uint8(71) //0x47
+   bLen := len(bytes)
+   for j := 0; j < bLen; j++ {
+   if bytes[j] == syncByte {
+   bytes = bytes[j:]
+   break
+   }
+   }
+   w := bufio.NewWriter(f)
+   if _, err := w.Write(bytes); err != nil {
+   return fmt.Errorf("write to %s: %s", fTemp, err.Error())
+   }
+   // Release file resource to rename file
+   _ = f.Close()
+   if err = os.Rename(fTemp, fPath); err != nil {
+   return err
+   }
+   // Maybe it will be safer in this way...
+   atomic.AddInt32(&d.finish, 1)
+   fmt.Printf("[download %6.2f%%] %s\n", float32(d.finish)/float32(d.segLen)*100, tsUrl)
+   return nil
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
 const (
    CryptMethodAES  CryptMethod = "AES-128"
    CryptMethodNONE CryptMethod = "NONE"
@@ -31,106 +112,6 @@ type Key struct {
 	IV     string
 }
 
-func AES128Encrypt(origData, key, iv []byte) ([]byte, error) {
-	block, err := aes.NewCipher(key)
-	if err != nil {
-		return nil, err
-	}
-	blockSize := block.BlockSize()
-	if len(iv) == 0 {
-		iv = key
-	}
-	origData = pkcs5Padding(origData, blockSize)
-	blockMode := cipher.NewCBCEncrypter(block, iv[:blockSize])
-	crypted := make([]byte, len(origData))
-	blockMode.CryptBlocks(crypted, origData)
-	return crypted, nil
-}
-
-func AES128Decrypt(crypted, key, iv []byte) ([]byte, error) {
-	block, err := aes.NewCipher(key)
-	if err != nil {
-		return nil, err
-	}
-	blockSize := block.BlockSize()
-	if len(iv) == 0 {
-		iv = key
-	}
-	blockMode := cipher.NewCBCDecrypter(block, iv[:blockSize])
-	origData := make([]byte, len(crypted))
-	blockMode.CryptBlocks(origData, crypted)
-	origData = pkcs5UnPadding(origData)
-	return origData, nil
-}
-
-func pkcs5Padding(cipherText []byte, blockSize int) []byte {
-	padding := blockSize - len(cipherText)%blockSize
-	padText := bytes.Repeat([]byte{byte(padding)}, padding)
-	return append(cipherText, padText...)
-}
-
-func pkcs5UnPadding(origData []byte) []byte {
-	length := len(origData)
-	unPadding := int(origData[length-1])
-	return origData[:(length - unPadding)]
-}
-
-func (d *Downloader) download(segIndex int) error {
-	tsFilename := tsFilename(segIndex)
-	tsUrl := d.tsURL(segIndex)
-	b, e := Get(tsUrl)
-	if e != nil {
-		return fmt.Errorf("request %s, %s", tsUrl, e.Error())
-	}
-	//noinspection GoUnhandledErrorResult
-	defer b.Close()
-	fPath := filepath.Join(d.tsFolder, tsFilename)
-	fTemp := fPath + tsTempFileSuffix
-	f, err := os.Create(fTemp)
-	if err != nil {
-		return fmt.Errorf("create file: %s, %s", tsFilename, err.Error())
-	}
-	bytes, err := ioutil.ReadAll(b)
-	if err != nil {
-		return fmt.Errorf("read bytes: %s, %s", tsUrl, err.Error())
-	}
-	sf := d.result.M3u8.Segments[segIndex]
-	if sf == nil {
-		return fmt.Errorf("invalid segment index: %d", segIndex)
-	}
-	key, ok := d.result.Keys[sf.KeyIndex]
-	if ok && key != "" {
-		bytes, err = AES128Decrypt(bytes, []byte(key),
-			[]byte(d.result.M3u8.Keys[sf.KeyIndex].IV))
-		if err != nil {
-			return fmt.Errorf("decryt: %s, %s", tsUrl, err.Error())
-		}
-	}
-	// https://en.wikipedia.org/wiki/MPEG_transport_stream
-	// Some TS files do not start with SyncByte 0x47, they can not be played after merging,
-	// Need to remove the bytes before the SyncByte 0x47(71).
-	syncByte := uint8(71) //0x47
-	bLen := len(bytes)
-	for j := 0; j < bLen; j++ {
-		if bytes[j] == syncByte {
-			bytes = bytes[j:]
-			break
-		}
-	}
-	w := bufio.NewWriter(f)
-	if _, err := w.Write(bytes); err != nil {
-		return fmt.Errorf("write to %s: %s", fTemp, err.Error())
-	}
-	// Release file resource to rename file
-	_ = f.Close()
-	if err = os.Rename(fTemp, fPath); err != nil {
-		return err
-	}
-	// Maybe it will be safer in this way...
-	atomic.AddInt32(&d.finish, 1)
-	fmt.Printf("[download %6.2f%%] %s\n", float32(d.finish)/float32(d.segLen)*100, tsUrl)
-	return nil
-}
 
 func FromURL(link string) (*Result, error) {
 	u, err := url.Parse(link)
