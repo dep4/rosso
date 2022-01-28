d41b9bdcd9711343991bbcf5578489d8f3859f31
Zombo <srpen6@gmail.com>
2022-01-27 19:30:37 -0600

    deleted:    research/2022-01-26/m3u.go

--- research/2022-01-27/m3u.go
+++ research/2022-01-27/m3u.go
@@ -1,12 +1,96 @@
 package m3u
 
 import (
+   "bytes"
    "crypto/aes"
    "crypto/cipher"
-   "github.com/89z/format"
    "io"
    "net/http"
+   "strconv"
 )
+
+type Format map[string]string
+
+func Unmarshal(buf []byte) []Format {
+   lines := bytes.FieldsFunc(buf, func(r rune) bool {
+      return r == '\n'
+   })
+   var pass1 []Format
+   for _, line := range lines {
+      if line[0] == '#' {
+         form := make(Format)
+         pairs := reader{line}
+         pairs.readBytes(':', '"')
+         for {
+            if pairs.buf == nil {
+               break
+            }
+            var pair reader
+            pair.buf = pairs.readBytes(',', '"')
+            key := pair.readBytes('=', '"')
+            if pair.buf != nil {
+               val := string(pair.buf)
+               unq, err := strconv.Unquote(val)
+               if err == nil {
+                  val = unq
+               }
+               form[string(key)] = val
+            }
+         }
+         pass1 = append(pass1, form)
+      } else {
+         ind := merge(pass1)
+         if ind >= 0 {
+            pass1[ind]["URI"] = string(line)
+         } else {
+            form := make(Format)
+            form["URI"] = string(line)
+            pass1 = append(pass1, form)
+         }
+      }
+   }
+   var pass2 []Format
+   uris := make(map[string]bool)
+   for _, form := range pass1 {
+      uri, ok := form["URI"]
+      if ok && !uris[uri] {
+         pass2 = append(pass2, form)
+         uris[uri] = true
+      }
+   }
+   return pass2
+}
+
+func merge(forms []Format) int {
+   if len(forms) >= 1 {
+      form := forms[len(forms)-1]
+      if len(form) >= 1 {
+         return len(forms)-1
+      }
+   }
+   return -1
+}
+
+type reader struct {
+   buf []byte
+}
+
+func (r *reader) readBytes(sep, enc byte) []byte {
+   out := true
+   for key, val := range r.buf {
+      if val == enc {
+         out = !out
+      }
+      if out && val == sep {
+         buf := r.buf[:key]
+         r.buf = r.buf[key+1:]
+         return buf
+      }
+   }
+   buf := r.buf
+   r.buf = nil
+   return buf
+}
 
 func unpad(buf []byte) []byte {
    total := len(buf)
@@ -20,7 +104,6 @@ func unpad(buf []byte) []byte {
 }
 
 func newDecrypter(req *http.Request) (cipher.BlockMode, error) {
-   logLevel.Dump(req)
    res, err := new(http.Transport).RoundTrip(req)
    if err != nil {
       return nil, err
@@ -35,21 +118,4 @@ func newDecrypter(req *http.Request) (cipher.BlockMode, error) {
       return nil, err
    }
    return cipher.NewCBCDecrypter(block, key), nil
-}
-
-var logLevel format.LogLevel
-
-func writeFile(req *http.Request, dec cipher.BlockMode) ([]byte, error) {
-   res, err := new(http.Transport).RoundTrip(req)
-   if err != nil {
-      return nil, err
-   }
-   defer res.Body.Close()
-   src, err := io.ReadAll(res.Body)
-   if err != nil {
-      return nil, err
-   }
-   dst := make([]byte, len(src))
-   dec.CryptBlocks(dst, src)
-   return unpad(dst), nil
 }
