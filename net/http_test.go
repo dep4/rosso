b18da0303d946580707440aed8df6fad622080fb
Zombo <srpen6@gmail.com>
2022-02-17 13:51:02 -0600

    deleted:    net/html_test.go

--- net/http_test.go
+++ net/http_test.go
@@ -7,9 +7,8 @@ import (
    "testing"
 )
 
-var sites = []string{
 // keep this as content-length is required
-`POST /api/v1/playlist/getFragment HTTP/1.1
+const post = `POST /api/v1/playlist/getFragment HTTP/1.1
 Host: pandora.com
 content-type:application/json
 cookie:csrftoken=842b12c83a3c5153
@@ -17,27 +16,14 @@ x-authtoken:BXoTKywEhnoiEqDEcu0U/qGlFBEK5Tjblz3fgnLPgFojficRTR8Xm6Lw==
 x-csrftoken:842b12c83a3c5153
 content-length: 54
 
-{"isStationStart":true,"stationId":126608766085892525}`,
-`POST /player/addToPlaylist HTTP/1.1
-Host: bleep.com
-Content-Type: application/x-www-form-urlencoded
-
-id=8728&type=ReleaseProduct`,
-`GET /manifest.json HTTP/1.1
-Host: github.com
-
-`,
-}
+{"isStationStart":true,"stationId":126608766085892525}`
 
 func TestRequest(t *testing.T) {
-   for _, site := range sites {
-      req, err := ReadRequest(strings.NewReader(site), true)
-      if err != nil {
-         t.Fatal(err)
-      }
-      if err := WriteRequest(req, os.Stdout); err != nil {
-         t.Fatal(err)
-      }
-      fmt.Println()
+   req, err := ReadRequest(strings.NewReader(post), true)
+   if err != nil {
+      t.Fatal(err)
+   }
+   if err := WriteRequest(req, os.Stdout); err != nil {
+      t.Fatal(err)
    }
 }
