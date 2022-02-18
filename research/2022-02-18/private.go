98ceb4946992cb3528d3d19b21c0d3ad0494f525
Zombo <srpen6@gmail.com>
2022-02-18 12:38:53 -0600

    new file:   research/2022-02-18/public.go

--- research/2022-02-18/private.go
+++ research/2022-02-18/private.go
@@ -3,23 +3,6 @@ package protobuf
 import (
    "github.com/89z/format"
    "google.golang.org/protobuf/encoding/protowire"
-   "strconv"
-)
-
-func (t Tag) MarshalText() ([]byte, error) {
-   var buf []byte
-   buf = strconv.AppendInt(buf, int64(t.Number), 10)
-   buf = append(buf, ' ')
-   buf = append(buf, t.Name...)
-   return buf, nil
-}
-
-const (
-   bytesType = iota * 0.1
-   fixed64Type
-   messageType
-   stringType
-   varintType
 )
 
 func appendField(buf []byte, num protowire.Number, val interface{}) []byte {
@@ -57,59 +40,8 @@ func consumeField(buf []byte) (float64, protowire.Type, int, error) {
    return float64(num), typ, fLen, protowire.ParseError(fLen)
 }
 
-type Message map[Tag]interface{}
-
-func Unmarshal(buf []byte) (Message, error) {
-   mes := make(Message)
-   for len(buf) >= 1 {
-      num, typ, fLen, err := consumeField(buf)
-      if err != nil {
-         return nil, err
-      }
-      _, _, tLen := protowire.ConsumeTag(buf[:fLen])
-      if err := protowire.ParseError(tLen); err != nil {
-         return nil, err
-      }
-      val := buf[tLen:fLen]
-      switch typ {
-      case protowire.BytesType:
-         err = mes.consumeBytes(num, val)
-      case protowire.Fixed64Type:
-         err = mes.consumeFixed64(num, val)
-      case protowire.VarintType:
-         err = mes.consumeVarint(num, val)
-      }
-      if err != nil {
-         return nil, err
-      }
-      buf = buf[fLen:]
-   }
-   return mes, nil
-}
-
-func (m Message) Add(num float64, name string, val Message) error {
-   tag := Tag{NumberType: num + messageType}
-   switch value := m[tag].(type) {
-   case nil:
-      m[tag] = val
-   case Message:
-      m[tag] = []Message{value, val}
-   case []Message:
-      m[tag] = append(value, val)
-   }
-   return nil
-}
-
-func (m Message) Marshal() []byte {
-   var buf []byte
-   for tag, val := range m {
-      buf = appendField(buf, protowire.Number(tag.NumberType), val)
-   }
-   return buf
-}
-
 func (m Message) addString(num float64, val string) {
-   tag := Tag{NumberType: num + stringType}
+   tag := Tag{NumberType: num + BytesType}
    switch value := m[tag].(type) {
    case nil:
       m[tag] = val
@@ -134,7 +66,7 @@ func (m Message) consumeBytes(num float64, buf []byte) error {
    mes, err := Unmarshal(val)
    if err != nil {
       if binary {
-         tag := Tag{NumberType: num + bytesType}
+         tag := Tag{NumberType: num + BytesType}
          switch value := m[tag].(type) {
          case nil:
             m[tag] = val
@@ -147,7 +79,7 @@ func (m Message) consumeBytes(num float64, buf []byte) error {
          m.addString(num, string(val))
       }
    } else {
-      m.Add(num, "", mes)
+      m.Add(num, mes)
       if !binary {
          m.addString(num, string(val))
       }
@@ -161,7 +93,7 @@ func (m Message) consumeFixed64(num float64, buf []byte) error {
    if err != nil {
       return err
    }
-   tag := Tag{NumberType: num + fixed64Type}
+   tag := Tag{NumberType: num + Fixed64Type}
    switch value := m[tag].(type) {
    case nil:
       m[tag] = val
@@ -179,7 +111,7 @@ func (m Message) consumeVarint(num float64, buf []byte) error {
    if err != nil {
       return err
    }
-   tag := Tag{NumberType: num + varintType}
+   tag := Tag{NumberType: num + VarintType}
    switch value := m[tag].(type) {
    case nil:
       m[tag] = val
@@ -189,9 +121,4 @@ func (m Message) consumeVarint(num float64, buf []byte) error {
       m[tag] = append(value, val)
    }
    return nil
-}
-
-type Tag struct {
-   NumberType float64
-   Name string
 }
