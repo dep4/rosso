0442f480c924977c6f7f64d9f570c00b058564fa
Zombo <srpen6@gmail.com>
2022-02-24 13:39:37 -0600

    deleted:    research/crypto/parse.go

--- crypto/parse.go
+++ crypto/parse.go
@@ -2,36 +2,59 @@ package crypto
 
 import (
    "fmt"
-   "github.com/89z/format"
    "github.com/refraction-networking/utls"
    "strings"
 )
 
 func ParseJA3(str string) (*tls.ClientHelloSpec, error) {
-   tokens := strings.Split(str, ",")
-   if tLen := len(tokens); tLen <= 4 {
-      return nil, format.InvalidSlice{4, tLen}
-   }
-   // uTLS does not support 0x0 as min version
-   hello := tls.ClientHelloSpec{TLSVersMin: tls.VersionTLS10}
-   // TLSVersMin is the record version, TLSVersMax is the handshake version
-   _, err := fmt.Sscan(tokens[0], &hello.TLSVersMax)
-   if err != nil {
-      return nil, err
-   }
-   // build CipherSuites
-   cipherKeys := strings.Split(tokens[1], "-")
-   for _, cipherKey := range cipherKeys {
-      var cipher uint16
-      _, err := fmt.Sscan(cipherKey, &cipher)
-      if err != nil {
-         return nil, err
+   var (
+      extensions string
+      info tls.ClientHelloInfo
+      spec tls.ClientHelloSpec
+   )
+   for i, field := range strings.SplitN(str, ",", 5) {
+      switch i {
+      case 0:
+         // TLSVersMin is the record version, TLSVersMax is the handshake
+         // version
+         _, err := fmt.Sscan(field, &spec.TLSVersMax)
+         if err != nil {
+            return nil, err
+         }
+      case 1:
+         // build CipherSuites
+         for _, cipherKey := range strings.Split(field, "-") {
+            var cipher uint16
+            _, err := fmt.Sscan(cipherKey, &cipher)
+            if err != nil {
+               return nil, err
+            }
+            spec.CipherSuites = append(spec.CipherSuites, cipher)
+         }
+      case 2:
+         extensions = field
+      case 3:
+         for _, curveKey := range strings.Split(field, "-") {
+            var curve tls.CurveID
+            _, err := fmt.Sscan(curveKey, &curve)
+            if err != nil {
+               return nil, err
+            }
+            info.SupportedCurves = append(info.SupportedCurves, curve)
+         }
+      case 4:
+         for _, pointKey := range strings.Split(field, "-") {
+            var point uint8
+            _, err := fmt.Sscan(pointKey, &point)
+            if err != nil {
+               return nil, err
+            }
+            info.SupportedPoints = append(info.SupportedPoints, point)
+         }
       }
-      hello.CipherSuites = append(hello.CipherSuites, cipher)
    }
    // build extenions list
-   extKeys := strings.Split(tokens[2], "-")
-   for _, extKey := range extKeys {
+   for _, extKey := range strings.Split(extensions, "-") {
       var ext tls.TLSExtension
       switch extKey {
       case "0":
@@ -41,29 +64,9 @@ func ParseJA3(str string) (*tls.ClientHelloSpec, error) {
          // Android API 26
          ext = &tls.StatusRequestExtension{}
       case "10":
-         var curves []tls.CurveID
-         curveKeys := strings.Split(tokens[3], "-")
-         for _, curveKey := range curveKeys {
-            var curve tls.CurveID
-            _, err := fmt.Sscan(curveKey, &curve)
-            if err != nil {
-               return nil, err
-            }
-            curves = append(curves, curve)
-         }
-         ext = &tls.SupportedCurvesExtension{curves}
+         ext = &tls.SupportedCurvesExtension{info.SupportedCurves}
       case "11":
-         var points []uint8
-         pointKeys := strings.Split(tokens[4], "-")
-         for _, pointKey := range pointKeys {
-            var point uint8
-            _, err := fmt.Sscan(pointKey, &point)
-            if err != nil {
-               return nil, err
-            }
-            points = append(points, point)
-         }
-         ext = &tls.SupportedPointsExtension{points}
+         ext = &tls.SupportedPointsExtension{info.SupportedPoints}
       case "13":
          ext = &tls.SignatureAlgorithmsExtension{
             SupportedSignatureAlgorithms: []tls.SignatureScheme{
@@ -104,7 +107,9 @@ func ParseJA3(str string) (*tls.ClientHelloSpec, error) {
          }
          ext = &tls.GenericExtension{Id: id}
       }
-      hello.Extensions = append(hello.Extensions, ext)
+      spec.Extensions = append(spec.Extensions, ext)
    }
-   return &hello, nil
+   // uTLS does not support 0x0 as min version
+   spec.TLSVersMin = tls.VersionTLS10
+   return &spec, nil
 }
