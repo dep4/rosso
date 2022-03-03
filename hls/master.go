94686d0400a6458516a48ac448238671c4d24c02
Zombo <srpen6@gmail.com>
2022-03-03 11:12:37 -0600

    new file:   hls/segment.go

--- hls/master.go
+++ hls/master.go
@@ -1,29 +1,11 @@
 package hls
 
 import (
-   "crypto/aes"
-   "crypto/cipher"
    "io"
    "net/url"
    "strconv"
    "text/scanner"
-   "unicode"
 )
-
-func (d Decrypter) Copy(dst io.Writer, src io.Reader) (int, error) {
-   buf, err := io.ReadAll(src)
-   if err != nil {
-      return 0, err
-   }
-   cipher.NewCBCDecrypter(d.Block, d.IV).CryptBlocks(buf, buf)
-   if len(buf) >= 1 {
-      pad := buf[len(buf)-1]
-      if len(buf) >= int(pad) {
-         buf = buf[:len(buf)-int(pad)]
-      }
-   }
-   return dst.Write(buf)
-}
 
 func NewMaster(addr *url.URL, body io.Reader) (*Master, error) {
    var (
@@ -102,85 +84,6 @@ func NewMaster(addr *url.URL, body io.Reader) (*Master, error) {
    return &mas, nil
 }
 
-func NewSegment(addr *url.URL, body io.Reader) (*Segment, error) {
-   var (
-      buf scanner.Scanner
-      err error
-      seg Segment
-   )
-   buf.Init(body)
-   for {
-      scanWords(&buf)
-      if buf.Scan() == scanner.EOF {
-         break
-      }
-      switch buf.TokenText() {
-      case "EXT-X-KEY":
-         for buf.Scan() != '\n' {
-            switch buf.TokenText() {
-            case "METHOD":
-               buf.Scan()
-               buf.Scan()
-               seg.Key.Method = buf.TokenText()
-            case "URI":
-               buf.Scan()
-               buf.Scan()
-               seg.Key.URI, err = strconv.Unquote(buf.TokenText())
-               if err != nil {
-                  return nil, err
-               }
-               addr, err := addr.Parse(seg.Key.URI)
-               if err != nil {
-                  return nil, err
-               }
-               seg.Key.URI = addr.String()
-            }
-         }
-      case "EXTINF":
-         var info Information
-         buf.Scan()
-         buf.Scan()
-         info.Duration = buf.TokenText()
-         scanLines(&buf)
-         buf.Scan()
-         buf.Scan()
-         addr, err := addr.Parse(buf.TokenText())
-         if err != nil {
-            return nil, err
-         }
-         info.URI = addr.String()
-         seg.Info = append(seg.Info, info)
-      }
-   }
-   return &seg, nil
-}
-
-func scanLines(buf *scanner.Scanner) {
-   buf.IsIdentRune = func(r rune, i int) bool {
-      return r != '\n'
-   }
-   buf.Whitespace = 1 << '\n'
-}
-
-func scanWords(buf *scanner.Scanner) {
-   buf.IsIdentRune = func(r rune, i int) bool {
-      return r == '-' || r == '.' || unicode.IsLetter(r) || unicode.IsDigit(r)
-   }
-   buf.Whitespace = 1 << ' '
-}
-
-func NewDecrypter(src io.Reader) (*Decrypter, error) {
-   key, err := io.ReadAll(src)
-   if err != nil {
-      return nil, err
-   }
-   block, err := aes.NewCipher(key)
-   if err != nil {
-      return nil, err
-   }
-   return &Decrypter{block, key}, nil
-}
-
 func (s Stream) String() string {
    var buf []byte
    if s.Resolution != "" {
@@ -201,4 +104,22 @@ func (s Stream) String() string {
       buf = append(buf, s.URI...)
    }
    return string(buf)
+}
+
+type Master struct {
+   Media []Media
+   Stream []Stream
+}
+
+type Media struct {
+   GroupID string
+   URI string
+}
+
+type Stream struct {
+   Resolution string
+   Bandwidth int64 // handle duplicate resolution
+   Codecs string // handle audio only
+   Audio string // link to Media
+   URI string
 }
