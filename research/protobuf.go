6f46bac9f1338c60c15702af041292fdc35bbabd
Zombo <srpen6@gmail.com>
2022-03-15 13:13:04 -0500

    new file:   research/protobuf_test.go

--- research/protobuf.go
+++ research/protobuf.go
@@ -3,18 +3,43 @@ package protobuf
 import (
    "github.com/89z/format"
    "google.golang.org/protobuf/encoding/protowire"
+   "strconv"
 )
 
-// We cannot include the name in the key. When you Unmarshal, the name will be
-// empty. If you then try to Get with a name, it will fail. Max valid number is
-// 536,870,911, so better to use float64:
-// stackoverflow.com/questions/3793838
-type Message map[Number]interface{}
+type Tag struct {
+   protowire.Number
+   protowire.Type
+}
+
+func (t Tag) MarshalText() ([]byte, error) {
+   buf := strconv.AppendInt(nil, int64(t.Number), 10)
+   switch t.Type {
+   case bytesType:
+      buf = append(buf, " bytes"...)
+   case fixed64Type:
+      buf = append(buf, " fixed64"...)
+   case messageType:
+      buf = append(buf, " message"...)
+   case varintType:
+      buf = append(buf, " varint"...)
+   }
+   return buf, nil
+}
+
+type Message map[Tag]interface{}
+
+const (
+   bytesType = 2
+   fixed64Type = 1
+   messageType = 6
+   varintType = 0
+)
 
 func Unmarshal(buf []byte) (Message, error) {
    mes := make(Message)
    for len(buf) >= 1 {
-      num, typ, fLen, err := consumeField(buf)
+      num, typ, fLen := protowire.ConsumeField(buf)
+      err := protowire.ParseError(fLen)
       if err != nil {
          return nil, err
       }
@@ -39,33 +64,67 @@ func Unmarshal(buf []byte) (Message, error) {
    return mes, nil
 }
 
-func (m Message) Add(num Number, name string, val Message) error {
-   num += messageType
-   switch value := m[num].(type) {
+func (m Message) consumeFixed64(num protowire.Number, buf []byte) error {
+   val, vLen := protowire.ConsumeFixed64(buf)
+   err := protowire.ParseError(vLen)
+   if err != nil {
+      return err
+   }
+   tag := Tag{num, fixed64Type}
+   switch value := m[tag].(type) {
    case nil:
-      m[num] = val
-   case Message:
-      m[num] = []Message{value, val}
-   case []Message:
-      m[num] = append(value, val)
+      m[tag] = val
+   case uint64:
+      m[tag] = []uint64{value, val}
+   case []uint64:
+      m[tag] = append(value, val)
    }
    return nil
 }
 
-type Number float64
+func (m Message) consumeVarint(num protowire.Number, buf []byte) error {
+   val, vLen := protowire.ConsumeVarint(buf)
+   err := protowire.ParseError(vLen)
+   if err != nil {
+      return err
+   }
+   tag := Tag{num, varintType}
+   switch value := m[tag].(type) {
+   case nil:
+      m[tag] = val
+   case uint64:
+      m[tag] = []uint64{value, val}
+   case []uint64:
+      m[tag] = append(value, val)
+   }
+   return nil
+}
 
-const (
-   messageType Number = 0
-   bytesType Number = 0.1
-   varintType Number = 0.2
-   fixed64Type Number = 0.3
-)
+func (m Message) addString(num protowire.Number, val string) {
+   tag := Tag{num, bytesType}
+   switch value := m[tag].(type) {
+   case nil:
+      m[tag] = val
+   case string:
+      m[tag] = []string{value, val}
+   case []string:
+      m[tag] = append(value, val)
+   }
+}
 
-// In some cases if input is binary, then result could be a Message or byte
-// slice. We assume for now its always a Message. If input is not binary, then
-// result could be a Message or string. Since its not possible to tell Message
-// from string, we just add both under the same number, each with its own type.
-func (m Message) consumeBytes(num Number, buf []byte) error {
+func (m Message) Add(num protowire.Number, val Message) {
+   tag := Tag{num, messageType}
+   switch value := m[tag].(type) {
+   case nil:
+      m[tag] = val
+   case Message:
+      m[tag] = []Message{value, val}
+   case []Message:
+      m[tag] = append(value, val)
+   }
+}
+
+func (m Message) consumeBytes(num protowire.Number, buf []byte) error {
    val, vLen := protowire.ConsumeBytes(buf)
    err := protowire.ParseError(vLen)
    if err != nil {
@@ -75,76 +134,23 @@ func (m Message) consumeBytes(num Number, buf []byte) error {
    mes, err := Unmarshal(val)
    if err != nil {
       if binary {
-         num += bytesType
-         switch value := m[num].(type) {
+         tag := Tag{num, bytesType}
+         switch value := m[tag].(type) {
          case nil:
-            m[num] = val
+            m[tag] = val
          case []byte:
-            m[num] = [][]byte{value, val}
+            m[tag] = [][]byte{value, val}
          case [][]byte:
-            m[num] = append(value, val)
+            m[tag] = append(value, val)
          }
       } else {
          m.addString(num, string(val))
       }
    } else {
-      m.Add(num, "", mes)
+      m.Add(num, mes)
       if !binary {
          m.addString(num, string(val))
       }
-   }
-   return nil
-}
-
-func consumeField(buf []byte) (Number, protowire.Type, int, error) {
-   num, typ, fLen := protowire.ConsumeField(buf)
-   return Number(num), typ, fLen, protowire.ParseError(fLen)
-}
-
-func (m Message) addString(num Number, val string) {
-   num += bytesType
-   switch value := m[num].(type) {
-   case nil:
-      m[num] = val
-   case string:
-      m[num] = []string{value, val}
-   case []string:
-      m[num] = append(value, val)
-   }
-}
-
-func (m Message) consumeFixed64(num Number, buf []byte) error {
-   val, vLen := protowire.ConsumeFixed64(buf)
-   err := protowire.ParseError(vLen)
-   if err != nil {
-      return err
-   }
-   num += fixed64Type
-   switch value := m[num].(type) {
-   case nil:
-      m[num] = val
-   case uint64:
-      m[num] = []uint64{value, val}
-   case []uint64:
-      m[num] = append(value, val)
-   }
-   return nil
-}
-
-func (m Message) consumeVarint(num Number, buf []byte) error {
-   val, vLen := protowire.ConsumeVarint(buf)
-   err := protowire.ParseError(vLen)
-   if err != nil {
-      return err
-   }
-   num += varintType
-   switch value := m[num].(type) {
-   case nil:
-      m[num] = val
-   case uint64:
-      m[num] = []uint64{value, val}
-   case []uint64:
-      m[num] = append(value, val)
    }
    return nil
 }
