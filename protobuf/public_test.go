9d5ff2c1ad5fe213d9e00a0833b6e490e3fefaa3
Zombo <srpen6@gmail.com>
2022-03-20 12:57:06 -0500

    deleted:    research/2022-03-20/public.go

--- protobuf/public_test.go
+++ protobuf/public_test.go
@@ -1,22 +1,38 @@
 package protobuf
 
 import (
+   "encoding/json"
    "fmt"
    "os"
    "testing"
 )
 
-func TestProtobuf(t *testing.T) {
-   names := []string{"checkin.txt", "details.txt"}
-   for _, name := range names {
-      buf, err := os.ReadFile(name)
-      if err != nil {
-         t.Fatal(err)
-      }
-      mes, err := Unmarshal(buf)
-      if err != nil {
-         t.Fatal(err)
-      }
-      fmt.Println(mes)
+func TestCheckin(t *testing.T) {
+   buf, err := os.ReadFile("checkin.txt")
+   if err != nil {
+      t.Fatal(err)
    }
+   mes, err := Unmarshal(buf)
+   if err != nil {
+      t.Fatal(err)
+   }
+   enc := json.NewEncoder(os.Stdout)
+   enc.SetIndent("", " ")
+   enc.Encode(mes)
+   fmt.Println(mes.GetUint64(7))
+}
+
+func TestDetails(t *testing.T) {
+   buf, err := os.ReadFile("details.txt")
+   if err != nil {
+      t.Fatal(err)
+   }
+   mes, err := Unmarshal(buf)
+   if err != nil {
+      t.Fatal(err)
+   }
+   enc := json.NewEncoder(os.Stdout)
+   enc.SetIndent("", " ")
+   enc.Encode(mes)
+   fmt.Printf("%q\n", mes.Get(1).Get(2).Get(4).GetString(5))
 }
