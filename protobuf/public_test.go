9c039edbac27037d66b322ab4891db9fe45b1283
Zombo <srpen6@gmail.com>
2022-03-21 21:53:54 -0500

    deleted:    research/public.go

--- protobuf/public_test.go
+++ protobuf/public_test.go
@@ -34,5 +34,9 @@ func TestDetails(t *testing.T) {
    enc := json.NewEncoder(os.Stdout)
    enc.SetIndent("", " ")
    enc.Encode(mes)
+   fmt.Println(len(buf), len(mes.Marshal()))
    fmt.Printf("%q\n", mes.Get(1).Get(2).Get(4).GetString(5))
+   for _, image := range mes.Get(1).Get(2).Get(4).GetMessages(10) {
+      fmt.Println(image)
+   }
 }
