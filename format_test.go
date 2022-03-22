f87a91a16805008123da5c31d7f1efa4bbb035b4
Zombo <srpen6@gmail.com>
2022-03-22 07:17:30 -0500

    +	google.golang.org/protobuf v1.28.0

--- format_test.go
+++ format_test.go
@@ -7,6 +7,17 @@ import (
    "testing"
 )
 
+func TestDecode(t *testing.T) {
+   var token struct {
+      Services, Token string
+   }
+   err := Decode(&token, "token.json")
+   if err != nil {
+      t.Fatal(err)
+   }
+   fmt.Printf("%+v\n", token)
+}
+
 func TestProgress(t *testing.T) {
    res, err := http.Get("http://speedtest.lax.hivelocity.net/100mb.file")
    if err != nil {
@@ -17,7 +28,7 @@ func TestProgress(t *testing.T) {
    io.ReadAll(pro)
 }
 
-func TestMeasure(t *testing.T) {
+func TestLabel(t *testing.T) {
    fmt.Println(LabelNumber(9_999))
    fmt.Println(LabelSize(9_999))
    fmt.Println(LabelRate(9_999))
--- go.mod
+++ go.mod
@@ -4,7 +4,7 @@ go 1.18
 
 require (
 	github.com/refraction-networking/utls v1.0.0
-	google.golang.org/protobuf v1.27.1
+	google.golang.org/protobuf v1.28.0
 )
 
 require (
