4526344b3aa226133ed6fbd2798b8830f01081c4
Zombo <srpen6@gmail.com>
2022-03-22 07:17:30 -0500

    +	google.golang.org/protobuf v1.28.0

--- format_test.go
+++ format_test.go
@@ -7,6 +7,18 @@ import (
    "testing"
 )
 
+type token struct {
+   Services, Token string
+}
+
+func TestDecode(t *testing.T) {
+   tok, err := Open[token]("token.json")
+   if err != nil {
+      t.Fatal(err)
+   }
+   fmt.Printf("%+v\n", tok)
+}
+
 func TestProgress(t *testing.T) {
    res, err := http.Get("http://speedtest.lax.hivelocity.net/100mb.file")
    if err != nil {
@@ -17,7 +29,7 @@ func TestProgress(t *testing.T) {
    io.ReadAll(pro)
 }
 
-func TestMeasure(t *testing.T) {
+func TestLabel(t *testing.T) {
    fmt.Println(LabelNumber(9_999))
    fmt.Println(LabelSize(9_999))
    fmt.Println(LabelRate(9_999))
