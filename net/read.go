e821f02f39b21e3e3281c33fba692f9206896812
Zombo <srpen6@gmail.com>
2022-04-03 12:16:54 -0500

    new file:   net/read.go

---
+++ net/read.go
@@ -0,0 +1,58 @@
+package net
+
+import (
+   "bufio"
+   "io"
+   "net/http"
+   "net/textproto"
+   "net/url"
+   "strconv"
+   "strings"
+)
+
+func ReadRequest(src io.Reader, https bool) (*http.Request, error) {
+   var req http.Request
+   text := textproto.NewReader(bufio.NewReader(src))
+   // .Method
+   sMethodPath, err := text.ReadLine()
+   if err != nil {
+      return nil, err
+   }
+   methodPath := strings.Fields(sMethodPath)
+   if len(methodPath) != 3 {
+      return nil, textproto.ProtocolError(sMethodPath)
+   }
+   req.Method = methodPath[0]
+   // .URL
+   addr, err := url.Parse(methodPath[1])
+   if err != nil {
+      return nil, err
+   }
+   req.URL = addr
+   // .URL.Scheme
+   if https {
+      req.URL.Scheme = "https"
+   } else {
+      req.URL.Scheme = "http"
+   }
+   // .URL.Host
+   head, err := text.ReadMIMEHeader()
+   if err != nil {
+      return nil, err
+   }
+   req.URL.Host = head.Get("Host")
+   // .Header
+   req.Header = http.Header(head)
+   // .ContentLength
+   sLength := head.Get("Content-Length")
+   if sLength != "" {
+      length, err := strconv.ParseInt(sLength, 10, 64)
+      if err != nil {
+         return nil, err
+      }
+      req.ContentLength = length
+   }
+   // .Body
+   req.Body = io.NopCloser(text.R)
+   return &req, nil
+}
