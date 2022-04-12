59d80b932e7a4ab6d0d56de1c70c4dfc79e6d669
Zombo <srpen6@gmail.com>
2022-04-12 16:10:51 -0500

    new file:   research/format.go

--- hls/hls_test.go
+++ hls/hls_test.go
@@ -36,44 +36,24 @@ func TestHex(t *testing.T) {
    }
 }
 
-////////////////////////////////////////////////////////////////////////////////
-
 const segmentURL = 
    "https://ga.video.cdn.pbs.org/videos/nature" +
    "/1803a543-5d57-41f6-81bf-d199448d45f6/2000281849/hd-16x9-mezzanine-1080p" +
    "/naat4008_r-hls-16x9-1080pAudio+Selector+1.m3u8"
 
-const masterURL =
-   "https://link.theplatform.com/s/dJ5BDC/media/guid/2198311517" +
-   "/uOkB1qnYXZXeAM34djVYNHje2_gK4mmO?formats=MPEG4,M3U"
-
 func TestSegment(t *testing.T) {
-   fmt.Println("GET", masterURL)
-   res, err := http.Get(masterURL)
+   fmt.Println("GET", segmentURL)
+   res, err := http.Get(segmentURL)
    if err != nil {
       t.Fatal(err)
    }
    defer res.Body.Close()
-   master, err := NewScanner(res.Body).Master(res.Request.URL)
+   seg, err := NewScanner(res.Body).Segment(res.Request.URL)
    if err != nil {
       t.Fatal(err)
    }
-   for _, stream := range master.Stream {
-      if stream.Bandwidth == 497000 {
-         fmt.Println("GET", stream.URI)
-         res, err := http.Get(stream.URI.String())
-         if err != nil {
-            t.Fatal(err)
-         }
-         defer res.Body.Close()
-         seg, err := NewScanner(res.Body).Segment(stream.URI)
-         if err != nil {
-            t.Fatal(err)
-         }
-         length := seg.Length(stream)
-         if length != 82456897 {
-            t.Fatal(length)
-         }
-      }
+   fmt.Println(len(seg.Info))
+   for _, info := range seg.Info {
+      fmt.Printf("%+v\n", info)
    }
 }
