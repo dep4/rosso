0d53e8b55ebce04779146c55ad29135fb597dadd
Zombo <srpen6@gmail.com>
2022-04-12 11:47:07 -0500

    +   "https://ga.video.cdn.pbs.org/videos/nature" +

--- hls/hls_test.go
+++ hls/hls_test.go
@@ -7,6 +7,42 @@ import (
    "testing"
 )
 
+func TestBandwidth(t *testing.T) {
+   master := &Master{Stream: []Stream{
+      {Bandwidth: 480},
+      {Bandwidth: 144},
+      {Bandwidth: 1080},
+      {Bandwidth: 720},
+      {Bandwidth: 2160},
+   }}
+   sort.Sort(Bandwidth{master, 720})
+   for _, str := range master.Stream {
+      fmt.Println(str)
+   }
+}
+
+var ivs = []string{
+   "00000000000000000000000000000001",
+   "0x00000000000000000000000000000001",
+}
+
+func TestHex(t *testing.T) {
+   for _, iv := range ivs {
+      buf, err := scanHex(iv)
+      if err != nil {
+         t.Fatal(err)
+      }
+      fmt.Println(buf)
+   }
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+const segmentURL = 
+   "https://ga.video.cdn.pbs.org/videos/nature" +
+   "/1803a543-5d57-41f6-81bf-d199448d45f6/2000281849/hd-16x9-mezzanine-1080p" +
+   "/naat4008_r-hls-16x9-1080pAudio+Selector+1.m3u8"
+
 const masterURL =
    "https://link.theplatform.com/s/dJ5BDC/media/guid/2198311517" +
    "/uOkB1qnYXZXeAM34djVYNHje2_gK4mmO?formats=MPEG4,M3U"
@@ -39,34 +75,5 @@ func TestSegment(t *testing.T) {
             t.Fatal(length)
          }
       }
-   }
-}
-
-func TestBandwidth(t *testing.T) {
-   master := &Master{Stream: []Stream{
-      {Bandwidth: 480},
-      {Bandwidth: 144},
-      {Bandwidth: 1080},
-      {Bandwidth: 720},
-      {Bandwidth: 2160},
-   }}
-   sort.Sort(Bandwidth{master, 720})
-   for _, str := range master.Stream {
-      fmt.Println(str)
-   }
-}
-
-var ivs = []string{
-   "00000000000000000000000000000001",
-   "0x00000000000000000000000000000001",
-}
-
-func TestHex(t *testing.T) {
-   for _, iv := range ivs {
-      buf, err := scanHex(iv)
-      if err != nil {
-         t.Fatal(err)
-      }
-      fmt.Println(buf)
    }
 }
