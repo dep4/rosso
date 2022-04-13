39d32b1001e18349758529a12c7572b3972cb484
Zombo <srpen6@gmail.com>
2022-04-13 10:41:02 -0500

    new file:   research/progress.go

--- progress.go
+++ progress.go
@@ -7,32 +7,6 @@ import (
    "time"
 )
 
-type Progress struct {
-   chunks int64
-   io.Writer
-   lapTime time.Time
-   length int
-   readChunks int64
-   readLength int64
-   time time.Time
-}
-
-func (p *Progress) AddChunk(length int64) {
-   p.readChunks += 1
-   p.readLength += length
-}
-
-func (p Progress) String() string {
-   rate := float64(p.length) / time.Since(p.time).Seconds()
-   var buf strings.Builder
-   buf.WriteString(Percent(p.length, p.chunks*p.readLength/p.readChunks))
-   buf.WriteByte('\t')
-   buf.WriteString(LabelSize(p.length))
-   buf.WriteByte('\t')
-   buf.WriteString(LabelRate(rate))
-   return buf.String()
-}
-
 func (p *Progress) Write(buf []byte) (int, error) {
    since := time.Since(p.lapTime)
    if since >= time.Second/2 {
@@ -41,8 +15,36 @@ func (p *Progress) Write(buf []byte) (int, error) {
       p.lapTime = p.lapTime.Add(since)
    }
    write, err := p.Writer.Write(buf)
-   p.length += write
+   p.bytes += write
    return write, err
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+func (p Progress) String() string {
+   rate := float64(p.bytes) / time.Since(p.time).Seconds()
+   var buf strings.Builder
+   buf.WriteString(Percent(p.bytes, p.chunks*p.bytesRead/p.chunksRead))
+   buf.WriteByte('\t')
+   buf.WriteString(LabelSize(p.bytes))
+   buf.WriteByte('\t')
+   buf.WriteString(LabelRate(rate))
+   return buf.String()
+}
+
+type Progress struct {
+   io.Writer
+   bytes int
+   bytesRead int64
+   chunks int64
+   chunksRead int64
+   time time.Time
+   lapTime time.Time
+}
+
+func (p *Progress) AddChunk(length int64) {
+   p.chunksRead += 1
+   p.bytesRead += length
 }
 
 func NewProgress(dst io.Writer, chunks int) *Progress {
