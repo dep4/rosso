971925e1fec9060453ad7489338f568f46e666f2
Zombo <srpen6@gmail.com>
2022-05-10 15:58:35 -0500

    new file:   research/dash.go

---
+++ research/dash_test.go
@@ -0,0 +1,29 @@
+package dash
+
+import (
+   "encoding/hex"
+   "os"
+   "testing"
+)
+
+const hexKey = "13d7c7cf295444944b627ef0ad2c1b3c"
+
+func TestDASH(t *testing.T) {
+   src, err := os.Open("ignore/enc.mp4")
+   if err != nil {
+      t.Fatal(err)
+   }
+   defer src.Close()
+   dst, err := os.Create("ignore/dec.mp4")
+   if err != nil {
+      t.Fatal(err)
+   }
+   defer dst.Close()
+   key, err := hex.DecodeString(hexKey)
+   if err != nil {
+      t.Fatal(err)
+   }
+   if err := decryptMP4withCenc(src, key, dst); err != nil {
+      t.Fatal(err)
+   }
+}
