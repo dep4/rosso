5a47a65fc0a5426f367e59c71a0ca0827d5385b4
Zombo <srpen6@gmail.com>
2022-05-11 14:27:28 -0500

    new file:   research/main.go

--- dash/decrypt_test.go
+++ dash/decrypt_test.go
@@ -2,11 +2,20 @@ package dash
 
 import (
    "encoding/hex"
+   "io"
    "os"
    "testing"
 )
 
 const hexKey = "13d7c7cf295444944b627ef0ad2c1b3c"
+
+func write(key []byte, dst io.Writer, name string) error {
+   file, err := os.Open(name)
+   if err != nil {
+      return err
+   }
+   return Decrypt(dst, file, key)
+}
 
 func TestDecrypt(t *testing.T) {
    dst, err := os.Create("ignore.mp4")
@@ -20,15 +29,14 @@ func TestDecrypt(t *testing.T) {
    }
    defer init.Close()
    dst.ReadFrom(init)
-   media, err := os.Open("ignore/enc.mp4")
-   if err != nil {
-      t.Fatal(err)
-   }
    key, err := hex.DecodeString(hexKey)
    if err != nil {
       t.Fatal(err)
    }
-   if err := Decrypt(dst, media, key); err != nil {
+   if err := write(key, dst, "ignore/1.mp4"); err != nil {
+      t.Fatal(err)
+   }
+   if err := write(key, dst, "ignore/2.mp4"); err != nil {
       t.Fatal(err)
    }
 }
