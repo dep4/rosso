c19c83df809060e19c1761b06554584e56334f29
Zombo <srpen6@gmail.com>
2022-06-05 10:06:49 -0500

    new file:   research/hls.go

--- hls/public_test.go
+++ hls/public_test.go
@@ -2,57 +2,27 @@ package hls
 
 import (
    "fmt"
-   "net/http"
    "net/url"
    "os"
    "testing"
 )
 
-const base = "https://play.itunes.apple.com" +
+const playlist = "https://play.itunes.apple.com" +
    "/WebObjects/MZPlay.woa/hls/subscription/playlist.m3u8"
 
 func TestSegment(t *testing.T) {
-   file, err := os.Open("m3u8/cbc-video.m3u8")
-   if err != nil {
-      return nil, err
-   }
-   defer file.Close()
-   addr, err := url.Parse(cbcSegment)
-   if err != nil {
-      return nil, err
-   }
-   return NewScanner(file).Segment(addr)
-   // FIXME
-   fmt.Println("GET", seg.Key)
-   res, err := http.Get(seg.Key.String())
-   if err != nil {
-      t.Fatal(err)
-   }
-   defer res.Body.Close()
-   block, err := NewCipher(res.Body)
-   if err != nil {
-      t.Fatal(err)
-   }
-   file, err := os.Create("ignore.mp4")
+   file, err := os.Open("ignore/apple-segment.m3u8")
    if err != nil {
       t.Fatal(err)
    }
    defer file.Close()
+   seg, err := NewScanner(file).Segment(&url.URL{})
+   if err != nil {
+      t.Fatal(err)
+   }
+   fmt.Println(seg.Key)
    for _, info := range seg.Info {
-      fmt.Println("GET", info.URI)
-      res, err := http.Get(info.URI.String())
-      if err != nil {
-         t.Fatal(err)
-      }
-      if res.StatusCode != http.StatusOK {
-         t.Fatal(res.Status)
-      }
-      if _, err := block.Copy(file, res.Body, info.IV); err != nil {
-         t.Fatal(err)
-      }
-      if err := res.Body.Close(); err != nil {
-         t.Fatal(err)
-      }
+      fmt.Printf("%+v\n", info)
    }
 }
 
@@ -62,7 +32,7 @@ func TestStream(t *testing.T) {
       t.Fatal(err)
    }
    defer file.Close()
-   base, err := url.Parse(base)
+   base, err := url.Parse(playlist)
    if err != nil {
       t.Fatal(err)
    }
@@ -88,7 +58,7 @@ func TestMedia(t *testing.T) {
       t.Fatal(err)
    }
    defer file.Close()
-   base, err := url.Parse(base)
+   base, err := url.Parse(playlist)
    if err != nil {
       t.Fatal(err)
    }
