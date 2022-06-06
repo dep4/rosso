b486cba75dc9923f2e0b54255b127b17b3bc7e6e
Zombo <srpen6@gmail.com>
2022-06-06 08:07:28 -0500

    deleted:    research/master.go

--- hls/segment.go
+++ hls/segment.go
@@ -3,14 +3,13 @@ package hls
 import (
    "crypto/aes"
    "crypto/cipher"
+   "encoding/base64"
+   "encoding/hex"
    "io"
    "net/url"
+   "strconv"
+   "strings"
    "text/scanner"
-)
-
-const (
-   AAC = ".aac"
-   TS = ".ts"
 )
 
 type Cipher struct {
@@ -48,47 +47,34 @@ func (c Cipher) Copy(w io.Writer, r io.Reader, iv []byte) (int, error) {
    return w.Write(buf)
 }
 
-type Information struct {
-   IV []byte
-   URI *url.URL
-}
-
-func (s *Scanner) Segment(addr *url.URL) (*Segment, error) {
+func (s Scanner) Segment() (*Segment, error) {
    var (
       info Information
       seg Segment
    )
-   for {
-      s.splitWords()
-      if s.Scan() == scanner.EOF {
-         break
-      }
-      var err error
-      switch s.TokenText() {
-      case "EXT-X-KEY":
-         for s.Scan() != '\n' {
+   for s.line.Scan() != scanner.EOF {
+      line := s.line.TokenText()
+      s.Init(strings.NewReader(line))
+      switch {
+      case strings.HasPrefix(line, "#EXT-X-KEY:"):
+         for s.Scan() != scanner.EOF {
             switch s.TokenText() {
             case "IV":
                s.Scan()
                s.Scan()
-               info.IV, err = scanHex(s.TokenText())
+               info.RawIV = s.TokenText()
             case "URI":
                s.Scan()
                s.Scan()
-               seg.Key, err = scanURL(s.TokenText(), addr)
-            }
-            if err != nil {
-               return nil, err
+               var err error
+               seg.RawKey, err = strconv.Unquote(s.TokenText())
+               if err != nil {
+                  return nil, err
+               }
             }
          }
-      case "EXTINF":
-         s.splitLines()
-         s.Scan()
-         s.Scan()
-         info.URI, err = addr.Parse(s.TokenText())
-         if err != nil {
-            return nil, err
-         }
+      case len(line) >= 1 && !strings.HasPrefix(line, "#"):
+         info.RawURI = line
          seg.Info = append(seg.Info, info)
          info = Information{}
       }
@@ -96,7 +82,33 @@ func (s *Scanner) Segment(addr *url.URL) (*Segment, error) {
    return &seg, nil
 }
 
+type Information struct {
+   RawIV string
+   RawURI string
+}
+
+func (i Information) IV() ([]byte, error) {
+   up := strings.ToUpper(i.RawIV)
+   return hex.DecodeString(strings.TrimPrefix(up, "0X"))
+}
+
+func (i Information) URI(base *url.URL) (*url.URL, error) {
+   return base.Parse(i.RawURI)
+}
+
 type Segment struct {
-   Key *url.URL
    Info []Information
+   RawKey string
+}
+
+func (s Segment) Key(base *url.URL) (*url.URL, error) {
+   return base.Parse(s.RawKey)
+}
+
+func (s Segment) PSSH() ([]byte, error) {
+   _, after, found := strings.Cut(s.RawKey, "data:text/plain;base64,")
+   if found {
+      s.RawKey = after
+   }
+   return base64.StdEncoding.DecodeString(s.RawKey)
 }
