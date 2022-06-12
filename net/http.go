06ceec95b912d0e4926fcb4f4011493136595da5
Zombo <srpen6@gmail.com>
2022-06-12 09:05:16 -0500

    renamed:    net/write.go -> net/http.go

--- net/http.go
+++ net/http.go
@@ -1,11 +1,14 @@
 package net
 
 import (
+   "bufio"
    "bytes"
    "fmt"
    "io"
    "net/http"
+   "net/textproto"
    "net/url"
+   "strings"
    "text/template"
 )
 
@@ -84,4 +87,45 @@ type requestTemplate struct {
    BodyIO string
    Query url.Values
    VarBody string
+}
+func ReadRequest(src io.Reader) (*http.Request, error) {
+   var req http.Request
+   text := textproto.NewReader(bufio.NewReader(src))
+   // .Method
+   sMethodPath, err := text.ReadLine()
+   if err != nil {
+      return nil, err
+   }
+   methodPath := strings.Fields(sMethodPath)
+   if len(methodPath) != 3 {
+      return nil, textproto.ProtocolError(sMethodPath)
+   }
+   req.Method = methodPath[0]
+   // .URL
+   addr, err := url.Parse(methodPath[1])
+   if err != nil {
+      return nil, err
+   }
+   req.URL = addr
+   // .URL.Host
+   head, err := text.ReadMIMEHeader()
+   if err != nil {
+      return nil, err
+   }
+   if req.URL.Host == "" {
+      req.URL.Host = head.Get("Host")
+   }
+   // .Header
+   req.Header = http.Header(head)
+   // .Body
+   buf := new(bytes.Buffer)
+   bLen, err := text.R.WriteTo(buf)
+   if err != nil {
+      return nil, err
+   }
+   if bLen >= 1 {
+      req.Body = io.NopCloser(buf)
+   }
+   req.ContentLength = bLen
+   return &req, nil
 }
