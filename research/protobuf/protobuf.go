d04163bd43aafcbdc983ee104ab2dad0cc933e40
Zombo <srpen6@gmail.com>
2022-06-21 14:28:19 -0500

    +type String string

--- research/protobuf/protobuf.go
+++ research/protobuf/protobuf.go
@@ -8,26 +8,48 @@ import (
    "strconv"
 )
 
-func (m Message) consume_raw(num Number, buf []byte) ([]byte, error) {
-   var (
-      length int
-      val Raw
-   )
-   val.Bytes, length = protowire.ConsumeBytes(buf)
+func add[T Encoder](mes Message, num Number, val T) error {
+   in := mes[num]
+   switch out := in.(type) {
+   case nil:
+      mes[num] = val
+   case T:
+      mes[num] = Slice[T]{out, val}
+   case Slice[T]:
+      mes[num] = append(out, val)
+   default:
+      return type_error{num, in, out}
+   }
+   return nil
+}
+
+func (m Message) consume_varint(num Number, buf []byte) ([]byte, error) {
+   val, length := protowire.ConsumeVarint(buf)
    err := protowire.ParseError(length)
    if err != nil {
       return nil, err
    }
-   val.Message, err = Unmarshal(val.Bytes)
-   if err != nil {
-      err = add(m, num, val.Bytes)
-   } else {
-      err = add(m, num, val)
-   }
-   if err != nil {
+   if err := add(m, num, Varint(val)); err != nil {
       return nil, err
    }
    return buf[length:], nil
+}
+
+func (m Message) Marshal() []byte {
+   var (
+      nums []Number
+      bufs []byte
+   )
+   for num := range m {
+      nums = append(nums, num)
+   }
+   sort.Slice(nums, func(a, b int) bool {
+      return nums[a] < nums[b]
+   })
+   for _, num := range nums {
+      bufs = m[num].encode(bufs, num)
+   }
+   return bufs
 }
 
 func (m Message) consume_fixed32(num Number, buf []byte) ([]byte, error) {
@@ -52,94 +74,6 @@ func (m Message) consume_fixed64(num Number, buf []byte) ([]byte, error) {
       return nil, err
    }
    return buf[length:], nil
-}
-
-func (m Message) consume_varint(num Number, buf []byte) ([]byte, error) {
-   val, length := protowire.ConsumeVarint(buf)
-   err := protowire.ParseError(length)
-   if err != nil {
-      return nil, err
-   }
-   if err := add(m, num, Varint(val)); err != nil {
-      return nil, err
-   }
-   return buf[length:], nil
-}
-
-type Raw struct {
-   Bytes Bytes
-   Message Message
-}
-
-func (m Message) Marshal() []byte {
-   var (
-      nums []Number
-      bufs []byte
-   )
-   for num := range m {
-      nums = append(nums, num)
-   }
-   sort.Slice(nums, func(a, b int) bool {
-      return nums[a] < nums[b]
-   })
-   for _, num := range nums {
-      bufs = m[num].encode(bufs, num)
-   }
-   return bufs
-}
-
-func (Raw) get_type() string { return "Raw" }
-
-func (r Raw) encode(buf []byte, num Number) []byte {
-   buf = protowire.AppendTag(buf, num, protowire.BytesType)
-   return protowire.AppendBytes(buf, r.Bytes)
-}
-
-func add[T Encoder](mes Message, num Number, val T) error {
-   in := mes[num]
-   switch out := in.(type) {
-   case nil:
-      mes[num] = val
-   case T:
-      mes[num] = Slice[T]{out, val}
-   case Slice[T]:
-      mes[num] = append(out, val)
-   default:
-      return type_error{num, in, out}
-   }
-   return nil
-}
-func Unmarshal(buf []byte) (Message, error) {
-   if len(buf) == 0 {
-      return nil, io.ErrUnexpectedEOF
-   }
-   mes := make(Message)
-   for len(buf) >= 1 {
-      num, typ, length := protowire.ConsumeTag(buf)
-      err := protowire.ParseError(length)
-      if err != nil {
-         return nil, err
-      }
-      buf = buf[length:]
-      switch typ {
-      case protowire.VarintType:
-         buf, err = mes.consume_varint(num, buf)
-      case protowire.Fixed64Type:
-         buf, err = mes.consume_fixed64(num, buf)
-      case protowire.Fixed32Type:
-         buf, err = mes.consume_fixed32(num, buf)
-      case protowire.BytesType:
-         buf, err = mes.consume_raw(num, buf)
-      case protowire.StartGroupType:
-      case protowire.EndGroupType:
-      default:
-         return nil, errors.New("cannot parse reserved wire type")
-      }
-      if err != nil {
-         return nil, err
-      }
-   }
-   return mes, nil
 }
 
 type Slice[T Encoder] []T
@@ -225,3 +159,72 @@ func (m Message) encode(buf []byte, num Number) []byte {
    return protowire.AppendBytes(buf, m.Marshal())
 }
 
+func Unmarshal(buf []byte) (Message, error) {
+   if len(buf) == 0 {
+      return nil, io.ErrUnexpectedEOF
+   }
+   mes := make(Message)
+   for len(buf) >= 1 {
+      num, typ, length := protowire.ConsumeTag(buf)
+      err := protowire.ParseError(length)
+      if err != nil {
+         return nil, err
+      }
+      buf = buf[length:]
+      switch typ {
+      case protowire.VarintType:
+         buf, err = mes.consume_varint(num, buf)
+      case protowire.Fixed64Type:
+         buf, err = mes.consume_fixed64(num, buf)
+      case protowire.Fixed32Type:
+         buf, err = mes.consume_fixed32(num, buf)
+      case protowire.BytesType:
+         buf, err = mes.consume_raw(num, buf)
+      case protowire.StartGroupType:
+      case protowire.EndGroupType:
+      default:
+         return nil, errors.New("cannot parse reserved wire type")
+      }
+      if err != nil {
+         return nil, err
+      }
+   }
+   return mes, nil
+}
+
+func (Raw) get_type() string { return "Raw" }
+
+func (r Raw) encode(buf []byte, num Number) []byte {
+   buf = protowire.AppendTag(buf, num, protowire.BytesType)
+   return protowire.AppendBytes(buf, r.Bytes)
+}
+
+type String string
+
+type Raw struct {
+   Bytes Bytes
+   String String
+   Message Message
+}
+
+func (m Message) consume_raw(num Number, buf []byte) ([]byte, error) {
+   var (
+      length int
+      val Raw
+   )
+   val.Bytes, length = protowire.ConsumeBytes(buf)
+   err := protowire.ParseError(length)
+   if err != nil {
+      return nil, err
+   }
+   val.Message, err = Unmarshal(val.Bytes)
+   if err != nil {
+      err = add(m, num, val.Bytes)
+   } else {
+      err = add(m, num, val)
+   }
+   if err != nil {
+      return nil, err
+   }
+   return buf[length:], nil
+}
