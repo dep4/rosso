1e9c8d0d2d6e358dcc7b1cb266048e754ff31eb7
Zombo <srpen6@gmail.com>
2022-06-22 11:27:21 -0500

    modified:   research/protobuf/protobuf.go

--- research/protobuf/protobuf.go
+++ research/protobuf/protobuf.go
@@ -1,177 +1,39 @@
-package protobuf
+package main
 
 import (
-   "errors"
-   "github.com/89z/format"
-   "google.golang.org/protobuf/encoding/protowire"
-   "io"
-   "strconv"
+   "fmt"
 )
 
-type Message map[Number]Encoder
+type message map[string]encoder
 
-type Encoded interface {
-   Slice_Varint
+type encoder interface {
+   encode()
 }
 
-type Slice_Varint []uint64
+type encoded interface {
+   uint32 | uint64
+}
 
-func (Slice_Varint) get_type() string { return "Slice_Varint" }
+type slice[T encoded] []T
 
-func add[T Encoded](mes Message, num Number, val T) error {
-   in := mes[num]
-   switch out := in.(type) {
+func (slice[T]) encode(){}
+
+func add[T encoded](mes message, key string, val T) {
+   switch out := mes[key].(type) {
    case nil:
-      mes[num] = Encoder(val)
-   case T:
-      mes[num] = Slice[T]{out, val}
-   case Slice[T]:
-      mes[num] = append(out, val)
+      mes[key] = slice[T]{val}
+   case slice[T]:
+      mes[key] = append(out, val)
    default:
-      return type_error{num, in, out}
+      fmt.Println("error", val)
    }
-   return nil
 }
 
-func (m Message) consume_varint(num Number, buf []byte) ([]byte, error) {
-   val, length := protowire.ConsumeVarint(buf)
-   err := protowire.ParseError(length)
-   if err != nil {
-      return nil, err
-   }
-   if err := add(m, num, Varint(val)); err != nil {
-      return nil, err
-   }
-   return buf[length:], nil
-}
-
-func Unmarshal(buf []byte) (Message, error) {
-   if len(buf) == 0 {
-      return nil, io.ErrUnexpectedEOF
-   }
-   mes := make(Message)
-   for len(buf) >= 1 {
-      num, typ, length := protowire.ConsumeTag(buf)
-      err := protowire.ParseError(length)
-      if err != nil {
-         return nil, err
-      }
-      buf = buf[length:]
-      switch typ {
-      case protowire.VarintType:
-         buf, err = mes.consume_varint(num, buf)
-      case protowire.Fixed64Type:
-         buf, err = mes.consume_fixed64(num, buf)
-      case protowire.Fixed32Type:
-         buf, err = mes.consume_fixed32(num, buf)
-      case protowire.BytesType:
-         buf, err = mes.consume_raw(num, buf)
-      default:
-         return nil, errors.New("cannot parse reserved wire type")
-      }
-      if err != nil {
-         return nil, err
-      }
-   }
-   return mes, nil
-}
-
-func (m Message) consume_fixed32(num Number, buf []byte) ([]byte, error) {
-   val, length := protowire.ConsumeFixed32(buf)
-   err := protowire.ParseError(length)
-   if err != nil {
-      return nil, err
-   }
-   if err := add(m, num, Fixed32(val)); err != nil {
-      return nil, err
-   }
-   return buf[length:], nil
-}
-
-func (m Message) consume_fixed64(num Number, buf []byte) ([]byte, error) {
-   val, length := protowire.ConsumeFixed64(buf)
-   err := protowire.ParseError(length)
-   if err != nil {
-      return nil, err
-   }
-   if err := add(m, num, Fixed64(val)); err != nil {
-      return nil, err
-   }
-   return buf[length:], nil
-}
-
-func (m Message) consume_raw(num Number, buf []byte) ([]byte, error) {
-   var (
-      length int
-      val Raw
-   )
-   val.Bytes, length = protowire.ConsumeBytes(buf)
-   err := protowire.ParseError(length)
-   if err != nil {
-      return nil, err
-   }
-   if format.String(val.Bytes) {
-      val.String = string(val.Bytes)
-   }
-   val.Message, _ = Unmarshal(val.Bytes)
-   if err := add(m, num, val); err != nil {
-      return nil, err
-   }
-   return buf[length:], nil
-}
-
-func (Message) get_type() string { return "Message" }
-
-type Bytes []byte
-
-func (Bytes) get_type() string { return "Bytes" }
-
-type Encoder interface {
-   get_type() string
-}
-
-type Fixed32 uint32
-
-func (Fixed32) get_type() string { return "Fixed32" }
-
-type Fixed64 uint64
-
-func (Fixed64) get_type() string { return "Fixed64" }
-
-type Number = protowire.Number
-
-type Raw struct {
-   Bytes []byte
-   String string
-   Message map[Number]Encoder
-}
-
-func (Raw) get_type() string { return "Raw" }
-
-type String string
-
-func (String) get_type() string { return "String" }
-
-type type_error struct {
-   Number
-   in Encoder
-   out Encoder
-}
-
-func (t type_error) Error() string {
-   var b []byte
-   b = append(b, "field "...)
-   b = strconv.AppendInt(b, int64(t.Number), 10)
-   b = append(b, " is "...)
-   b = append(b, t.in.get_type()...)
-   b = append(b, ", not "...)
-   b = append(b, t.out.get_type()...)
-   return string(b)
-}
-
-type Slice[T Encoded] []T
-
-func (Slice[T]) get_type() string {
-   var value T
-   return "[]" + Encoder(value).get_type()
+func main() {
+   mes := make(message)
+   add(mes, "one", uint64(1))
+   add(mes, "one", uint64(2))
+   add(mes, "one", uint64(3))
+   add(mes, "one", uint32(4))
+   fmt.Println(mes)
 }
