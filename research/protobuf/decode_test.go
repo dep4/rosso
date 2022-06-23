d519e4bdc706f78df28b901e4d2a639199ba31bb
Zombo <srpen6@gmail.com>
2022-06-23 10:44:08 -0500

    new file:   research/protobuf/decode.go

---
+++ research/protobuf/decode_test.go
@@ -0,0 +1,69 @@
+package protobuf
+
+import (
+   "bufio"
+   "os"
+   "testing"
+   
+   "encoding/json"
+)
+
+func TestCheckin(t *testing.T) {
+   file, err := os.Open("com.pinterest.txt")
+   if err != nil {
+      t.Fatal(err)
+   }
+   defer file.Close()
+   responseWrapper, err := Decode(bufio.NewReader(file))
+   if err != nil {
+      t.Fatal(err)
+   }
+   docV2 := responseWrapper.Get(1).Get(2).Get(4)
+   {
+      // v := docV2.Get(13).Get(1).GetMessages(17)
+      v := docV2.Get(13).Get(1)
+      enc := json.NewEncoder(os.Stdout)
+      enc.SetIndent("", " ")
+      enc.Encode(v)
+   }
+   if v, err := docV2.Get(13).Get(1).GetVarint(3); err != nil {
+      t.Fatal(err)
+   } else if v != 10218030 {
+      t.Fatal("VersionCode", v)
+   }
+   if v, err := docV2.Get(13).Get(1).GetString(4); err != nil {
+      t.Fatal(err)
+   } else if v != "10.21.0" {
+      t.Fatal("VersionString", v)
+   }
+   if v, err := docV2.Get(13).Get(1).GetVarint(9); err != nil {
+      t.Fatal(err)
+   } else if v != 47705639 {
+      t.Fatal("Size", v)
+   }
+   if v, err := docV2.Get(13).Get(1).GetString(16); err != nil {
+      t.Fatal(err)
+   } else if v != "Jun 14, 2022" {
+      t.Fatal("Date", v)
+   }
+   if v, err := docV2.GetString(5); err != nil {
+      t.Fatal(err)
+   } else if v != "Pinterest" {
+      t.Fatal("title", v)
+   }
+   if v, err := docV2.GetString(6); err != nil {
+      t.Fatal(err)
+   } else if v != "Pinterest" {
+      t.Fatal("creator", v)
+   }
+   if v, err := docV2.Get(8).GetString(2); err != nil {
+      t.Fatal(err)
+   } else if v != "USD" {
+      t.Fatal("currencyCode", v)
+   }
+   if v, err := docV2.Get(13).Get(1).GetVarint(70); err != nil {
+      t.Fatal(err)
+   } else if v != 750510010 {
+      t.Fatal("NumDownloads", v)
+   }
+}
