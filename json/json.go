9c36611c14a22928e8115c896a8436e50fb99637
Zombo <srpen6@gmail.com>
2022-06-27 16:56:37 -0500

    +   enc.SetIndent("", " ")

--- json/json.go
+++ json/json.go
@@ -3,36 +3,21 @@ package json
 import (
    "bytes"
    "encoding/json"
-   "github.com/89z/format"
-   "os"
+   "io"
 )
 
 var (
+   Marshal = json.Marshal
    NewDecoder = json.NewDecoder
    NewEncoder = json.NewEncoder
+   Unmarshal = json.Unmarshal
 )
 
-// `value` is the receiver
-func Create[T any](value T, name string) error {
-   file, err := format.Create(name)
-   if err != nil {
-      return err
-   }
-   defer file.Close()
-   return json.NewEncoder(file).Encode(value)
-}
-
-func Open[T any](name string) (*T, error) {
-   file, err := os.Open(name)
-   if err != nil {
-      return nil, err
-   }
-   defer file.Close()
-   value := new(T)
-   if err := json.NewDecoder(file).Decode(value); err != nil {
-      return nil, err
-   }
-   return value, nil
+func Encode(w io.Writer, value any) error {
+   enc := json.NewEncoder(w)
+   enc.SetEscapeHTML(false)
+   enc.SetIndent("", " ")
+   return enc.Encode(value)
 }
 
 type Scanner struct {
