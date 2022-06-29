eaf79c39a7be6388956d1b1dd91efeb3d1ae237f
Zombo <srpen6@gmail.com>
2022-06-28 22:22:24 -0500

    new file:   mp4/mp4.go

--- mp4/mp4_test.go
+++ mp4/mp4_test.go
@@ -1,4 +1,4 @@
-package dash
+package mp4
 
 import (
    "bytes"
@@ -15,19 +15,17 @@ type test_type struct {
 }
 
 var tests = []test_type{
-   // pass
    {
       "680a46ebd6cf2b9a6a0b05a24dcf944a",
       "ignore/enc-piff.mp4", "ignore/dec-piff.mp4",
    },
-   // fail
    {
       "22bdb0063805260307ee5045c0f3835a",
       "ignore/enc-cbcs.mp4", "ignore/dec-cbcs.mp4",
    },
 }
 
-func Test_Two(t *testing.T) {
+func Test_Decrypt(t *testing.T) {
    for _, test := range tests {
       fmt.Println(test.enc)
       file, err := os.Create(test.dec)
