a27339dbc5437168080ea45c14691405ef86eea5
Zombo <srpen6@gmail.com>
2022-07-01 09:36:40 -0500

    new file:   strconv/strconv.go

--- os/os.go
+++ os/os.go
@@ -1,11 +1,66 @@
 package os
 
 import (
+   "github.com/89z/std/strconv"
+   "io"
    "os"
    "path/filepath"
    "strings"
+   "time"
 )
 
+type Progress struct {
+   bytes int64
+   bytes_read int64
+   bytes_written int
+   chunks int
+   chunks_read int64
+   time time.Time
+   time_lap time.Time
+   w io.Writer
+}
+
+func (p *Progress) Write(buf []byte) (int, error) {
+   if p.time.IsZero() {
+      p.time = time.Now()
+      p.time_lap = time.Now()
+   }
+   since := time.Since(p.time_lap)
+   if since >= time.Second {
+      os.Stderr.WriteString(p.String())
+      os.Stderr.WriteString("\n")
+      p.time_lap = p.time_lap.Add(since)
+   }
+   write, err := p.w.Write(buf)
+   p.bytes_written += write
+   return write, err
+}
+
+func (p Progress) String() string {
+   ratio := strconv.Percent(p.bytes_written, p.bytes)
+   rate := float64(p.bytes_written) / time.Since(p.time).Seconds()
+   var buf strings.Builder
+   buf.WriteString(ratio)
+   buf.WriteByte('\t')
+   buf.WriteString(strconv.Size(p.bytes_written))
+   buf.WriteByte('\t')
+   buf.WriteString(strconv.Rate(rate))
+   return buf.String()
+}
+
+func Progress_Bytes(dst io.Writer, bytes int64) *Progress {
+   return &Progress{w: dst, bytes: bytes}
+}
+
+func Progress_Chunks(dst io.Writer, chunks int) *Progress {
+   return &Progress{w: dst, chunks: chunks}
+}
+
+func (p *Progress) Add_Chunk(bytes int64) {
+   p.bytes_read += bytes
+   p.chunks_read += 1
+   p.bytes = int64(p.chunks) * p.bytes_read / p.chunks_read
+}
 var (
    Open = os.Open
    ReadFile = os.ReadFile
