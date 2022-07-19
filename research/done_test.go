ff8bb456f2764238f4d64ce3bbb60bd82c654e21
Zombo <srpen6@gmail.com>
2022-07-18 20:31:26 -0500

    new file:   research/todo.go

--- research/done_test.go
+++ research/done_test.go
@@ -7,11 +7,11 @@ import (
 
 func Test_Append(t *testing.T) {
    var dst []byte
-   dst = AppendPercent(dst, Ratio(2, 3))
+   dst = NewRatio(2, 3).AppendPercent(dst)
    dst = append(dst, '\n')
    dst = AppendCardinal(dst, 9999)
    dst = append(dst, '\n')
-   dst = AppendRate(dst, Ratio(12345, 10))
+   dst = NewRatio(12345, 10).AppendRate(dst)
    dst = append(dst, '\n')
    os.Stdout.Write(dst)
 }
