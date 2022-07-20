f1b6178193dbd8227005f90bf2aabc37bef618f1
Zombo <srpen6@gmail.com>
2022-07-20 01:24:55 -0500

    +         u, err := strconv.ParseUint(field, 10, 16)

--- crypto/scan.go
+++ crypto/scan.go
@@ -1,9 +1,8 @@
-// TLS and JA3 parsers
 package crypto
 
 import (
-   "fmt"
    "github.com/refraction-networking/utls"
+   "strconv"
    "strings"
 )
 
@@ -18,46 +17,44 @@ func Parse_JA3(str string) (*tls.ClientHelloSpec, error) {
       case 0:
          // TLSVersMin is the record version, TLSVersMax is the handshake
          // version
-         _, err := fmt.Sscan(field, &spec.TLSVersMax)
+         u, err := strconv.ParseUint(field, 10, 16)
          if err != nil {
             return nil, err
          }
+         spec.TLSVersMax = uint16(u)
       case 1:
          // build CipherSuites
-         for _, raw_cipher := range strings.Split(field, "-") {
-            var cipher uint16
-            _, err := fmt.Sscan(raw_cipher, &cipher)
+         for _, s := range strings.Split(field, "-") {
+            u, err := strconv.ParseUint(s, 10, 16)
             if err != nil {
                return nil, err
             }
-            spec.CipherSuites = append(spec.CipherSuites, cipher)
+            spec.CipherSuites = append(spec.CipherSuites, uint16(u))
          }
       case 2:
          extensions = field
       case 3:
-         for _, raw_curve := range strings.Split(field, "-") {
-            var curve tls.CurveID
-            _, err := fmt.Sscan(raw_curve, &curve)
+         for _, s := range strings.Split(field, "-") {
+            u, err := strconv.ParseUint(s, 10, 16)
             if err != nil {
                return nil, err
             }
-            info.SupportedCurves = append(info.SupportedCurves, curve)
+            info.SupportedCurves = append(info.SupportedCurves, tls.CurveID(u))
          }
       case 4:
-         for _, raw_point := range strings.Split(field, "-") {
-            var point uint8
-            _, err := fmt.Sscan(raw_point, &point)
+         for _, s := range strings.Split(field, "-") {
+            u, err := strconv.ParseUint(s, 10, 8)
             if err != nil {
                return nil, err
             }
-            info.SupportedPoints = append(info.SupportedPoints, point)
+            info.SupportedPoints = append(info.SupportedPoints, uint8(u))
          }
       }
    }
    // build extenions list
-   for _, raw_ID := range strings.Split(extensions, "-") {
+   for _, s := range strings.Split(extensions, "-") {
       var ext tls.TLSExtension
-      switch raw_ID {
+      switch s {
       case "0":
          // Android API 24
          ext = &tls.SNIExtension{}
@@ -106,12 +103,11 @@ func Parse_JA3(str string) (*tls.ClientHelloSpec, error) {
          // Android API 24
          ext = &tls.RenegotiationInfoExtension{}
       default:
-         var id uint16
-         _, err := fmt.Sscan(raw_ID, &id)
+         u, err := strconv.ParseUint(s, 10, 16)
          if err != nil {
             return nil, err
          }
-         ext = &tls.GenericExtension{Id: id}
+         ext = &tls.GenericExtension{Id: uint16(u)}
       }
       spec.Extensions = append(spec.Extensions, ext)
    }
