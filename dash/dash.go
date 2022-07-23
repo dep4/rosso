7c2cdefec624aa2b5e11f6b56bf5fb991bdc9b7e
Zombo <srpen6@gmail.com>
2022-07-23 13:56:52 -0500

    deleted:    research/http.go

--- dash/dash.go
+++ dash/dash.go
@@ -1,42 +1,52 @@
 package dash
 
 import (
-   "net/url"
    "strconv"
    "strings"
 )
 
-func (r Representation) Initialization(base *url.URL) (*url.URL, error) {
-   ref := r.replace_ID(r.SegmentTemplate.Initialization)
-   return base.Parse(ref)
-}
-
-func (r Representation) Media(base *url.URL) ([]*url.URL, error) {
-   var start int
-   if r.SegmentTemplate.StartNumber != nil {
-      start = *r.SegmentTemplate.StartNumber
-   }
-   var refs []*url.URL
-   for _, seg := range r.SegmentTemplate.SegmentTimeline.S {
-      for seg.T = start; seg.R >= 0; seg.R-- {
-         raw := r.replace_ID(r.SegmentTemplate.Media)
-         if r.SegmentTemplate.StartNumber != nil {
-            raw = strings.Replace(raw, "$Number$", seg.Time(), 1)
-            seg.T++
-            start++
-         } else {
-            raw = strings.Replace(raw, "$Time$", seg.Time(), 1)
-            seg.T += seg.D
-            start += seg.D
-         }
-         ref, err := base.Parse(raw)
-         if err != nil {
-            return nil, err
-         }
-         refs = append(refs, ref)
+func (r Representations) Filter(f func(Representation) bool) Representations {
+   var carry []Representation
+   for _, item := range r {
+      if f(item) {
+         carry = append(carry, item)
       }
    }
-   return refs, nil
+   return carry
+}
+
+func (r Representations) Video() Representations {
+   return r.Filter(func(a Representation) bool {
+      return a.MimeType == "video/mp4"
+   })
+}
+
+func (r Representations) Audio() Representations {
+   return r.Filter(func(a Representation) bool {
+      return a.MimeType == "audio/mp4"
+   })
+}
+
+func (r Representations) Index(f func(a, b Representation) bool) int {
+   carry := -1
+   for i, item := range r {
+      if carry == -1 || f(r[carry], item) {
+         carry = i
+      }
+   }
+   return carry
+}
+
+func (r Representations) Bandwidth(v int64) int {
+   distance := func(a Representation) int64 {
+      if a.Bandwidth > v {
+         return a.Bandwidth - v
+      }
+      return v - a.Bandwidth
+   }
+   return r.Index(func(carry, item Representation) bool {
+      return distance(item) < distance(carry)
+   })
 }
 
 func (r Representation) String() string {
